7b0b46469cc0a4ad87799266716ab2f2ce87799d
Avi Mehta <avimehta@google.com>
2017-10-03 11:06:26 -0700

    Fix compile step dependencies (#30)

--- build-system/tasks/compile.js
+++ build-system/tasks/compile.js
@@ -43,22 +43,25 @@ exports.compile = function(opts) {
   mkdirSync('build/fake-module');
   mkdirSync('build/fake-module/src');
   mkdirSync('build/css');
-  var promises = [
-    // Compile CSS
-    compileCss('./src/', './build/css', Object.assign({}, opts || {})),
 
-    // For compilation with babel we start with the main-babel entry point,
-    // but then rename to the subscriptions.js which we've been using all along.
-    compileJs('./src/', 'main', './dist', Object.assign({
-      toName: 'subscriptions.max.js',
-      minifiedName: 'subscriptions.js',
-      includePolyfills: true,
-      // If there is a sync JS error during initial load,
-      // at least try to unhide the body.
-      wrapper: '(function(){<%= contents %>})();'
-    }, opts || {})),
-  ];
-  return Promise.all(promises);
+  // Compile CSS because we need the css files in compileJs step.
+  return compileCss('./src/', './build/css', Object.assign({}, opts || {}))
+    .then(() => {
+
+      // For compilation with babel we start with the main-babel entry point,
+      // but then rename to the subscriptions.js which we've been using all along.
+      return Promise.all([
+        compileJs('./src/', 'main', './dist',
+          Object.assign({
+            toName: 'subscriptions.max.js',
+            minifiedName: 'subscriptions.js',
+            includePolyfills: true,
+            // If there is a sync JS error during initial load,
+            // at least try to unhide the body.
+            wrapper: '(function(){<%= contents %>})();'
+          }, opts || {})),
+        ]);
+    });
 }
 
 
