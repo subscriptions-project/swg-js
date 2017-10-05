f815d47145bffd6385a54eb97c6e773945ae837e
Dima Voytenko <dvoytenko@gmail.com>
2017-10-04 17:14:12 -0700

    force reload for re-auth

--- src/experimental/subscriptions-ui-flow.js
+++ src/experimental/subscriptions-ui-flow.js
@@ -211,7 +211,10 @@ export class SubscriptionsUiFlow {
   /** @private */
   paymentComplete_() {
     this.close_();
-    // TODO(avimehta): Restart authorization again.
+    // TODO(avimehta): Restart authorization again, instead of redirect here.
+    // (btw, it's fine if authorization restart does redirect itself when
+    // needed)
+    this.win_.location.reload(/* force */ true);
   }
 
   /**
