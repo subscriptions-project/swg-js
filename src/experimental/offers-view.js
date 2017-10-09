70dd0ce618e29e75a3843df77845db5d80cbb139
dparikh <dipak.parikh@gmail.com>
2017-10-09 14:57:03 -0700

    Rename AbbreviatedOffers to Offers (#65)
    * Rename AbbreviatedOffers to OffersView

--- src/experimental/offers-view.js
+++ src/experimental/offers-view.js
@@ -15,16 +15,16 @@
  */
 
 
-import {getAbbreviatedOffers} from './subscriptions-ui-util';
+import {getOffers} from './subscriptions-ui-util';
 import {setImportantStyles} from '../utils/style';
 
 
 /**
- * Abbreviated Offers view. Renders the content in the parent <swg-popup>
+ *  Offers view. Renders the content in the parent <swg-popup>
  * element. Checks the state of <swg-popup> element to ensure that the no other
  * element exists. Hides the loading indicator once rendered.
  */
-export class AbbreviatedOffersUi {
+export class OffersView {
 
   /**
    * @param {!Window} win The parent window object.
@@ -50,7 +50,7 @@ export class AbbreviatedOffersUi {
     this.subscriptions_ = subscriptions;
 
     /** @private @const {!Element} */
-    this.abbreviatedOffersElement_ = this.document_.createElement('iframe');
+    this.offersElement_ = this.document_.createElement('iframe');
 
     /** @private {?function()} */
     this.subscribeClicked_ = null;
@@ -61,7 +61,7 @@ export class AbbreviatedOffersUi {
 
   /**
    * @param {function()} callback
-   * @return {!AbbreviatedOffersUi}
+   * @return {!OffersView}
    */
   onSubscribeClicked(callback) {
     this.subscribeClicked_ = callback;
@@ -72,43 +72,43 @@ export class AbbreviatedOffersUi {
    * @return {!Element}
    */
   getElement() {
-    return this.abbreviatedOffersElement_;
+    return this.offersElement_;
   }
 
   /**
-   * Initializes the abbreviated offers and renders in the <swg-popup>.
+   * Initializes the offers and renders in the <swg-popup>.
    * @return {!Promise}
    */
   init() {
-    return this.buildAbbreviatedOffers_(this.subscriptions)
+    return this.buildOffers_(this.subscriptions)
         .then(() => this.show());
   }
 
   /*
-   * Shows the abbreviated offers element within the <swg-popup> element.
+   * Shows the offers element within the <swg-popup> element.
    */
   show() {
-    this.abbreviatedOffersElement_.style.removeProperty('display');
+    this.offersElement_.style.removeProperty('display');
   }
 
   /*
-   * Hides the abbreviated offers element within the <swg-popup> element.
+   * Hides the  offers element within the <swg-popup> element.
    */
   hide() {
-    this.abbreviatedOffersElement_.style
+    this.offersElement_.style
         .setProperty('display', 'none', 'important');
   }
 
   /*
-   * Builds the abbreviated offers element within the <swg-popup> element.
+   * Builds the  offers element within the <swg-popup> element.
    * @return {!Promise}
    * @private
    */
-  buildAbbreviatedOffers_() {
-    const iframe = this.abbreviatedOffersElement_;
+  buildOffers_() {
+    const iframe = this.offersElement_;
      // TODO(dparikh): Polyfill 'srcdoc'.
      // Ref.: https://github.com/ampproject/amphtml/blob/master/src/friendly-iframe-embed.js#L148-L163
-    iframe.srcdoc = getAbbreviatedOffers(this.subscriptions_);
+    iframe.srcdoc = getOffers(this.subscriptions_);
     iframe.setAttribute('frameborder', 0);
     iframe.setAttribute('scrolling', 'no');
 
@@ -151,7 +151,7 @@ export class AbbreviatedOffersUi {
    * @private
    */
   boundResizeListener_(event) {
-    const iframe = this.abbreviatedOffersElement_;
+    const iframe = this.offersElement_;
     const height = iframe.contentDocument.body.scrollHeight;
     this.context_.resizeView(this, height);
     event.currentTarget.removeEventListener(event.type, this.ref_);
