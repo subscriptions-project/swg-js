54efd0fced882ef2f7e740a3b1882ecb5a290863
dparikh <dipak.parikh@gmail.com>
2017-12-12 15:27:51 -0800

    Move AddBottomPadding() to dialog.js from subscriptions-flow (#167)
    * move AddBottomPadding() to dialog.js from subscriptions-flow

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -41,32 +41,55 @@ const rootElementImportantStyles = {
   'background-color': 'rgb(255, 255, 255)',
   'font-family': 'Roboto, sans-serif',
   'position': 'fixed',
-  'bottom': '0',
   'z-index': '2147483647',
   'box-shadow': 'gray 0px 3px, gray 0px 0px 22px',
   'box-sizing': 'border-box',
 };
 
+/**
+ * Position of the dialog.
+ * @const @enum {string}
+ */
+const PositionAt = {
+  BOTTOM: 'BOTTOM',
+  TOP: 'TOP',
+  FLOAT: 'FLOAT',
+  FULL: 'FULL',
+};
+
 
 /**
  * The class for the top level dialog.
+ * @final
  */
 export class Dialog {
 
   /**
-   * @param {!Document} doc
+   * Create a dialog with optionally provided window and override important
+   * styles and position styles.
+   * @param {!Window} win
+   * @param {!Object<string, string|number>=} importantStyles
+   * @param {!Object<string, string|number>=} styles
    */
-  constructor(doc) {
+  constructor(win, importantStyles = {}, styles = {}) {
+
+    /** @private @const {!Window} */
+    this.win_ = win;
+
     /** @private @const {!Document} */
-    this.doc_ = doc;
+    this.doc_ = this.win_.document;
 
     /** @private @const {!FriendlyIframe} */
-    this.iframe_ = new FriendlyIframe(doc, {'class': 'swg-dialog'});
+    this.iframe_ = new FriendlyIframe(this.doc_, {'class': 'swg-dialog'});
 
+    const modifiedImportantStyles =
+        Object.assign({}, rootElementImportantStyles, importantStyles);
     setImportantStyles(
-        this.iframe_.getElement(), rootElementImportantStyles);
+        this.iframe_.getElement(), modifiedImportantStyles);
 
-    setStyles(this.iframe_.getElement(), topFriendlyIframePositionStyles);
+    const modifiedStyles =
+        Object.assign({}, topFriendlyIframePositionStyles, styles);
+    setStyles(this.iframe_.getElement(), modifiedStyles);
 
     /** @private {?Element} */
     this.container_ = null;  // Depends on constructed document inside iframe.
@@ -101,6 +124,8 @@ export class Dialog {
     this.container_ =
         createElement(iframeDoc, 'div', {'class': 'swg-container'});
     iframe.getBody().appendChild(this.container_);
+    this.setPosition_();
+    this.updatePaddingToHtml_();
     return this;
   }
 
@@ -109,6 +134,7 @@ export class Dialog {
    */
   close() {
     this.doc_.body.removeChild(this.iframe_.getElement());
+    this.removePaddingToHtml_();
   }
 
   /**
@@ -136,5 +162,92 @@ export class Dialog {
    */
   getElement() {
     return this.iframe_.getElement();
+  }
+
+  /**
+   * Gets the element's height.
+   * @return {number}
+   * @private
+   */
+  getHeight_() {
+    return this.getElement().offsetHeight;
+  }
+
+  /**
+   * Sets the position of the dialog. Currently 'BOTTOM' is set by default.
+   * @return {string}
+   */
+  setPosition_() {
+    setImportantStyles(this.getElement(), this.getPositionStyle_());
+  }
+
+  /**
+   * Add the padding to the containing page so as to not hide the content
+   * behind the popup, if rendered at the bottom.
+   * @private
+   */
+  updatePaddingToHtml_() {
+    if (this.inferPosition_() == PositionAt.BOTTOM) {
+      const bottomPadding = this.getHeight_() + 20;  // Add some extra padding.
+      const htmlElement = this.doc_.documentElement;
+
+      // TODO(dparikh): Read the existing padding with the unit value
+      // (em, ex, %, px, cm, mm, in, pt, pc), and if available then append the
+      // padding after converting the units.
+      setImportantStyles(htmlElement, {
+        'padding-bottom': `${bottomPadding}px`,
+      });
+    }
+  }
+
+  /**
+   * Removes previouly added bottom padding from the document.
+   * @private`
+   */
+  removePaddingToHtml_() {
+    this.doc_.documentElement.style.removeProperty('padding-bottom');
+  }
+
+
+  /**
+   * Calculates the position of the dialog. Currently dialog is positioned at
+   * the bottom only. This could change in future to adjust the dialog position
+   * based on the screen size.
+   * @return {string}
+   * @private
+   */
+  inferPosition_() {
+    return PositionAt.BOTTOM;
+  }
+
+  /**
+   * Returns the styles required to postion the dialog.
+   * @return {!Object<string, string|number>}
+   * @private
+   */
+  getPositionStyle_() {
+    const dialogPosition = this.inferPosition_();
+    switch (dialogPosition) {
+      case PositionAt.BOTTOM:
+        return {'bottom': 0};
+      case PositionAt.TOP:
+        return {'top': 0};
+      case PositionAt.FLOAT:
+        return {
+          'position': 'fixed',
+          'top': '50%',
+          'left': '50%',
+          'transform': 'translate(-50%, -50%)',
+        };
+      case PositionAt.FULL:
+        return {
+          'position': 'fixed',
+          'height': '100%',
+          'top': 0,
+          'bottom': 0,
+        };
+      default:
+        return {'bottom': 0};
+    }
   }
 }
