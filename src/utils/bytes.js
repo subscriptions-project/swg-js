20d679869258d63d9135ba17caf124d7368acb05
Dima Voytenko <dvoytenko@gmail.com>
2018-01-19 14:38:27 -0800

    JWT decoder implementation and tests

--- src/utils/bytes.js
+++ src/utils/bytes.js
@@ -14,6 +14,18 @@
  * limitations under the License.
  */
 
+/**
+ * Character mapping from base64url to base64.
+ * @const {!Object<string, string>}
+ */
+const base64UrlDecodeSubs = {'-': '+', '_': '/', '.': '='};
+
+/**
+ * Character mapping from base64 to base64url.
+ * @const {!Object<string, string>}
+ */
+const base64UrlEncodeSubs = {'+': '-', '/': '_', '=': '.'};
+
 
 /**
  * Converts a string which holds 8-bit code points, such as the result of atob,
@@ -30,7 +42,38 @@ export function stringToBytes(str) {
     bytes[i] = charCode;
   }
   return bytes;
-};
+}
+
+
+/**
+ * Converts a 8-bit bytes array into a string
+ * @param {!Uint8Array} bytes
+ * @return {string}
+ */
+export function bytesToString(bytes) {
+  // Intentionally avoids String.fromCharCode.apply so we don't suffer a
+  // stack overflow. #10495, https://jsperf.com/bytesToString-2
+  const array = new Array(bytes.length);
+  for (let i = 0; i < bytes.length; i++) {
+    array[i] = String.fromCharCode(bytes[i]);
+  }
+  return array.join('');
+}
+
+
+/**
+ * Interpret a byte array as a UTF-8 string.
+ * @param {!BufferSource} bytes
+ * @return {string}
+ */
+export function utf8DecodeSync(bytes) {
+  if (typeof TextDecoder !== 'undefined') {
+    return new TextDecoder('utf-8').decode(bytes);
+  }
+  const asciiString = bytesToString(new Uint8Array(bytes.buffer || bytes));
+  return decodeURIComponent(escape(asciiString));
+}
+
 
 /**
  * Turn a string into UTF-8 bytes.
@@ -44,3 +87,26 @@ export function utf8EncodeSync(string) {
   return stringToBytes(unescape(encodeURIComponent(string)));
 }
 
+
+/**
+ * Converts a string which is in base64url encoding into a Uint8Array
+ * containing the decoded value.
+ * @param {string} str
+ * @return {!Uint8Array}
+ */
+export function base64UrlDecodeToBytes(str) {
+  const encoded = atob(str.replace(/[-_.]/g, ch => base64UrlDecodeSubs[ch]));
+  return stringToBytes(encoded);
+}
+
+
+/**
+ * Converts a bytes array into base64url encoded string.
+ * base64url is defined in RFC 4648. It is sometimes referred to as "web safe".
+ * @param {!Uint8Array} bytes
+ * @return {string}
+ */
+export function base64UrlEncodeFromBytes(bytes) {
+  const str = bytesToString(bytes);
+  return btoa(str).replace(/[+/=]/g, ch => base64UrlEncodeSubs[ch]);
+}
