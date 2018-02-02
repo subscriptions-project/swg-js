c5658fc56ee708a1ec1d2361ef48f1f7aa207b8c
Dima Voytenko <dvoytenko@gmail.com>
2018-02-02 15:39:57 -0800

    Cleanup

--- src/experimental/utils.js
+++ src/experimental/utils.js
@@ -14,9 +14,6 @@
  * limitations under the License.
  */
 
-import {CSS as OFFERS_CSS} from
-    '../../build/css/experimental/swg-popup-offer.css';
-
 /**
  * Maximum value for z-index (32 bit Integer).
  * @const {number}
@@ -26,30 +23,6 @@ export const MAX_Z_INDEX = 2147483647;
 /** @const {string} */
 export const IFRAME_CLASS = 'swg-iframe';
 
-/** @const {string} */
-export const SWG_OFFER_CONTENT_CLASS = 'swg-content';
-
-/** @const {string} */
-export const SWG_OFFER_ITEM_CLASS = 'swg-offer-item';
-
-
-/**
- * Renders offers view. Called from the subscriptions flow.
- */
-export function renderOffersView(subscriptions) {
-  const abbreviatedView =
-    `
-      <html>
-        <head>${getStyle()}</head>
-        <body>
-          <div class="swg-container">
-            ${renderOffersViewContent_(subscriptions)}
-          </div>
-        </body>
-      </html>
-    `;
-  return abbreviatedView;
-}
 
 /**
  * Renders abbreviated view. Called from the subscriptions flow.
@@ -77,31 +50,8 @@ export function renderAbbreviatedView(subscriptions) {
  */
 export function getStyle() {
   const fonts = '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">';
-  const style = `${fonts}<style>${OFFERS_CSS}</style> `;
+  const style = `${fonts}`;
   return style;
-}
-
-/**
- * Renders offers view content.
- * @private
- */
-function renderOffersViewContent_(subscriptions) {
-  const offers = subscriptions.offer;
-  const types = ['Basic', 'Premium'];
-  let offerContent = '';
-  for (let i = 0; i < offers.length; i++) {
-    offerContent += `
-      <div class="${SWG_OFFER_ITEM_CLASS}" data-offer-index="${i}"
-          tabindex="1" role="button">
-        <div class="swg-offer-item-header">${types[i]} subscription</div>
-        <div class="swg-offer-item-detail">${offers[i]['displayString']}</div>
-      </div>
-    `;
-  }
-  return `
-      <div class="swg-offer-header">Select an offer to continue</div>
-      <div>${offerContent}</div>
-  `;
 }
 
 /**
