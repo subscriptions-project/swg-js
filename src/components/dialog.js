d78ce3bac04d2d278aee0fef8f7d7f0f4f86d592
dparikh <dipak.parikh@gmail.com>
2018-02-09 12:38:48 -0800

    UX Changes - Move close action to the left and make optional

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -175,11 +175,6 @@ export class Dialog {
         createElement(iframeDoc, 'div', {'class': 'swg-container'});
     iframeBody.appendChild(this.container_);
     this.setPosition_();
-
-    // Inject the close button after the iframe for mouse click event to
-    // respond otherwisethe mouse click event is captured by iframe.
-    this.addCloseAction_(iframeDoc, iframeBody);
-    this.showCloseAction(this.showCloseAction_);
   }
 
   /**
@@ -278,6 +273,15 @@ export class Dialog {
       removeChildren(this.getContainer());
     }
     this.view_ = view;
+
+    const iframe = this.iframe_;
+    const iframeBody = iframe.getBody();
+    const iframeDoc = /** @type {!HTMLDocument} */ (this.iframe_.getDocument());
+
+    if (view.shouldShowCloseAction()) {
+      this.addCloseAction_(iframeDoc, iframeBody);
+      this.showCloseAction(this.showCloseAction_);
+    }
     setImportantStyles(view.getElement(), resetViewStyles);
     this.setLoading(true);
     this.getContainer().appendChild(view.getElement());
