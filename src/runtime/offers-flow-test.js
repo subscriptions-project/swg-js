6677002954bf120f220a61d2c0798810ab5e7ebf
Dima Voytenko <dvoytenko@google.com>
2018-03-07 13:35:29 -0800

    Subscribe option flow (#276)

--- src/runtime/offers-flow-test.js
+++ src/runtime/offers-flow-test.js
@@ -16,17 +16,23 @@
 
 import {ActivityPort} from 'web-activities/activity-ports';
 import {ConfiguredRuntime} from './runtime';
-import {OffersFlow} from './offers-flow';
+import {
+  OffersFlow,
+  SubscribeOptionFlow,
+} from './offers-flow';
 import {PageConfig} from '../model/page-config';
+import {PayStartFlow} from './pay-flow';
 import * as sinon from 'sinon';
 
-describes.realWin('Offers flow', {}, env => {
+
+describes.realWin('OffersFlow', {}, env => {
   let win;
   let offersFlow;
   let runtime;
   let activitiesMock;
   let callbacksMock;
   let pageConfig;
+  let port;
 
   beforeEach(() => {
     win = env.win;
@@ -35,6 +41,10 @@ describes.realWin('Offers flow', {}, env => {
     activitiesMock = sandbox.mock(runtime.activities());
     callbacksMock = sandbox.mock(runtime.callbacks());
     offersFlow = new OffersFlow(runtime);
+    port = new ActivityPort();
+    port.onResizeRequest = () => {};
+    port.onMessage = () => {};
+    port.whenReady = () => Promise.resolve();
   });
 
   afterEach(() => {
@@ -43,18 +53,96 @@ describes.realWin('Offers flow', {}, env => {
   });
 
   it('should have valid OffersFlow constructed', () => {
-    const port = new ActivityPort();
-    port.onResizeRequest = () => {};
-    port.onMessage = () => {};
-    port.whenReady = () => Promise.resolve();
     activitiesMock.expects('openIframe').withExactArgs(
         sinon.match(arg => arg.tagName == 'IFRAME'),
-        sinon.match.string,
+        '$frontend$/swglib/offersiframe$frontendDebug$',
         {
           publicationId: 'pub1',
           productId: 'pub1:label1',
         })
         .returns(Promise.resolve(port));
     return offersFlow.start();
+  });
+
+  it('should activate pay and linking flow', () => {
+    let messageCallback;
+    sandbox.stub(port, 'onMessage', callback => {
+      messageCallback = callback;
+    });
+    const payStub = sandbox.stub(PayStartFlow.prototype, 'start');
+    const loginStub = sandbox.stub(runtime.callbacks(), 'triggerLoginRequest');
+    activitiesMock.expects('openIframe').returns(Promise.resolve(port));
+    return offersFlow.start().then(() => {
+      // Unrlated message.
+      messageCallback({});
+      expect(payStub).to.not.be.called;
+      expect(loginStub).to.not.be.called;
+      // Pay message.
+      messageCallback({'sku': 'sku1'});
+      expect(payStub).to.be.calledOnce;
+      expect(loginStub).to.not.be.called;
+      // Login message.
+      messageCallback({'alreadySubscribed': true});
+      expect(loginStub).to.be.calledOnce;
+      expect(payStub).to.be.calledOnce;  // Dind't change.
+    });
+  });
+});
+
+
+describes.realWin('SubscribeOptionFlow', {}, env => {
+  let win;
+  let offersFlow;
+  let runtime;
+  let activitiesMock;
+  let callbacksMock;
+  let pageConfig;
+  let port;
+
+  beforeEach(() => {
+    win = env.win;
+    pageConfig = new PageConfig('pub1:label1');
+    runtime = new ConfiguredRuntime(win, pageConfig);
+    activitiesMock = sandbox.mock(runtime.activities());
+    callbacksMock = sandbox.mock(runtime.callbacks());
+    offersFlow = new SubscribeOptionFlow(runtime);
+    port = new ActivityPort();
+    port.onResizeRequest = () => {};
+    port.onMessage = () => {};
+    port.whenReady = () => Promise.resolve();
+  });
+
+  afterEach(() => {
+    activitiesMock.verify();
+    callbacksMock.verify();
+  });
+
+  it('should have valid SubscribeOptionFlow constructed', () => {
+    activitiesMock.expects('openIframe').withExactArgs(
+        sinon.match(arg => arg.tagName == 'IFRAME'),
+        '$frontend$/swglib/optionsiframe$frontendDebug$',
+        {
+          publicationId: 'pub1',
+        })
+        .returns(Promise.resolve(port));
+    return offersFlow.start();
+  });
+
+  it('should trigger offers flow when accepted', () => {
+    let messageCallback;
+    sandbox.stub(port, 'onMessage', callback => {
+      messageCallback = callback;
+    });
+    const offersStartStub = sandbox.stub(OffersFlow.prototype, 'start');
+    activitiesMock.expects('openIframe').returns(Promise.resolve(port));
+    expect(offersStartStub).to.not.be.called;
+    return offersFlow.start().then(() => {
+      // Unrelated message.
+      messageCallback({});
+      expect(offersStartStub).to.not.be.called;
+      // Subscribe message.
+      messageCallback({'subscribe': true});
+      expect(offersStartStub).to.be.calledOnce;
+    });
   });
 });
