0fbc6cd4983a87f76fa45fbeced214e8feda6411
Malte Ubl <cramforce@users.noreply.github.com>
2018-06-15 21:00:35 -0700

    Add additional URLs to prefetch and preconnect. (#360)
    As of today this should help a lot with getting things up and running quicker.
    The change does add a maintainability burden, so it should be considered whether this is worth it, or if a more sustainable strategy is feasible.

--- src/runtime/pay-flow.js
+++ src/runtime/pay-flow.js
@@ -65,6 +65,12 @@ export class PayStartFlow {
    */
   static preconnect(pre) {
     pre.prefetch(payUrl());
+    pre.prefetch(
+        'https://payments.google.com/payments/v4/js/integrator.js?ss=md');
+    pre.prefetch('https://clients2.google.com/gr/gr_full_2.0.6.js');
+    pre.preconnect('https://www.gstatic.com/');
+    pre.preconnect('https://fonts.googleapis.com/');
+    pre.preconnect('https://www.google.com/');
   }
 
   /**
