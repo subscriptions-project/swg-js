aefe838c8d42d1ceaffc9199c710e791cf870230
Dima Voytenko <dvoytenko@gmail.com>
2018-09-14 14:57:46 -0700

    Disable pay prefetch

--- src/runtime/pay-flow.js
+++ src/runtime/pay-flow.js
@@ -64,7 +64,6 @@ export class PayStartFlow {
    * @param {!../utils/preconnect.Preconnect} pre
    */
   static preconnect(pre) {
-    pre.prefetch(payUrl());
     pre.prefetch(
         'https://payments.google.com/payments/v4/js/integrator.js?ss=md');
     pre.prefetch('https://clients2.google.com/gr/gr_full_2.0.6.js');
