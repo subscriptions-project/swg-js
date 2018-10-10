d39c3d1112aed11a8163d78245df0f4b4b9434f3
dparikh <dipak.parikh@gmail.com>
2018-10-10 15:37:08 -0700

    Added config

--- build-system/tasks/karma.conf.js
+++ build-system/tasks/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = {
   preprocessors: {
     'src/**/*.js': ['browserify'],
     'test/**/*.js': ['browserify'],
-    'third_party/**/*.js': ['browserify'],
+    'third_party/random_uuid/*.js': ['browserify'],
   },
 
   browserify: {
