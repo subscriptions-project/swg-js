d90cdef0505372aaaf02990370e0b297674d3c4e
Chen Shay <chenshay@chenshay-macbookpro2.roam.corp.google.com>
2018-11-06 11:28:57 -0800

    fix

--- src/api/subscriptions.js
+++ src/api/subscriptions.js
@@ -269,13 +269,13 @@ export const ReplaceSkuProrationMode = {
 
 /**
  * String values input by the publisher are mapped to the number values.
- * @enum {string}
+ * @enum {number}
  */
 export const ReplaceSkuProrationModeMapping = {
   // The replacement takes effect immediately, and the remaining time will
   // be prorated and credited to the user. This is the current default
   // behavior.
-  IMMEDIATE_WITH_TIME_PRORATION: '1',
+  IMMEDIATE_WITH_TIME_PRORATION: 1,
 };
 
 
