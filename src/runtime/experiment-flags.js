1e9232227973a4c093941284a2ca295f28d69ea4
Dipak <dipak.parikh@gmail.com>
2019-01-02 10:34:40 -0800

    Added Contributions flow  behind experiments flag (#504)
    * Added Contributions flow  behind experiments flag

--- src/runtime/experiment-flags.js
+++ src/runtime/experiment-flags.js
@@ -36,4 +36,9 @@ export const ExperimentFlags = {
    * for another in the subscribe() API.
    */
   REPLACE_SUBSCRIPTION: 'replace-subscription',
+
+  /**
+   * Enables the contributions feature.
+   */
+  CONTRIBUTIONS: 'contributions',
 };
