e9b09134de48c3f1e6c60a356582a8595c4c817f
sohanirao <35511558+sohanirao@users.noreply.github.com>
2019-04-08 16:27:27 -0700

    Use Fetch API directly to send HTTP requests (#528)
    Utilize windows fetch API to send requests.

--- src/runtime/propensity-server.js
+++ src/runtime/propensity-server.js
@@ -13,99 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {parseJson} from '../utils/json';
-export class XhrInterface {
-  // TODO(sohanirao): Remove this class when productionalizing
-  /**
-   * Map of error code to error message
-   * @param {!Object.<number, string>} errorMap
-   */
-  constructor(errorMap) {
-    this.errorMap_ = errorMap;
-  }
-
-  /**
-   * Send request
-   * @param {string} url
-   * @param {!../utils/xhr.FetchInitDef} init
-   * @return {!Promise<!Response>}
-   */
-  sendRequest(url, init) {
-    return new Promise((resolve, reject) => {
-      /** @type {XMLHttpRequest} */
-      const xhr = new XMLHttpRequest();
-      const method = init.method || 'GET';
-      xhr.open(method, url, true);
-      if (init.headers) {
-        Object.keys(init.headers).forEach(function(header) {
-          xhr.setRequestHeader(header, init.headers[header]);
-        });
-      }
-      if (init.credentials) {
-        xhr.withCredentials = true;
-      }
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState < /* STATUS_RECEIVED */ 2) {
-          return;
-        }
-        if (xhr.readyState == /* COMPLETE */ 4) {
-          if (xhr.status in this.errorMap_) {
-            let errorMessage = xhr.status + ':' + this.errorMap_[xhr.status];
-            if (xhr.statusText) {
-              errorMessage = errorMessage + ' ' + xhr.statusText;
-            }
-            reject(new Error(errorMessage));
-          } else if (xhr.status < 200 || xhr.status > 300) {
-            const errorMessage = xhr.status + ':' + xhr.statusText;
-            reject(new Error(errorMessage));
-          } else {
-            const response = new Response();
-            response.status = xhr.status;
-            response.ok = true;
-            response.statusText = xhr.statusText;
-            xhr.bodyUsed = true;
-            response.responseText = xhr.responseText;
-            response.url = xhr.responseURL;
-            response.headers = this.getResponseHeader_(
-                xhr.getAllResponseHeaders());
-            resolve(response);
-          }
-        }
-      };
-      xhr.onerror = () => {
-        reject(new Error('Network failure'));
-      };
-      xhr.onabort = () => {
-        reject(new Error('Request aborted'));
-      };
-      if (init.method == 'POST') {
-        xhr.send(init.body);
-      } else {
-        xhr.send();
-      }
-    });
-  }
-
-  /**
-   * Creates header from XHR header
-   * @param {?string} responseHeaders
-   * @return {!Headers}
-   */
-  getResponseHeader_(responseHeaders) {
-    const headerMap = new Headers();
-    if (!responseHeaders) {
-      return headerMap;
-    }
-    const arr = responseHeaders.trim().split(/[/r/n]+/);
-    arr.forEach(function(line) {
-      const parts = line.split(': ');
-      const header = parts.shift();
-      const value = parts.join(': ');
-      headerMap[header] = value;
-    });
-    return headerMap;
-  }
-}
+import {Xhr} from '../utils/xhr';
+import * as ServiceUrl from './services';
 
 /**
  * Implements interface to Propensity server
@@ -126,52 +35,37 @@ export class PropensityServer {
     this.clientId_ = null;
     /** @private {boolean} */
     this.userConsent_ = false;
-    /** @private @const {boolean}*/
-    this.TEST_SERVERS_ = true;
-    /** {!Object.<number, string>} */
-    const errorMap = {
-      404: 'Publisher not whitelisted',
-      403: 'Not sent from allowed origin',
-      400: 'Invalid request',
-      500: 'Server not available',
-    };
-    /** @private @const {!XhrInterface} */
-    this.xhrInterface_ = new XhrInterface(errorMap);
+    /** @private @const {!Xhr} */
+    this.xhr_ = new Xhr(win);
   }
 
   /**
+   * @private
    * @return {string}
    */
-  getUrl_() {
-    if (this.TEST_SERVERS_) {
-      return 'http://sohanirao.mtv.corp.google.com:8080';
-    } else {
-      return 'https://pubads.g.doubleclick.net/gampad/adx';
-    }
+  getDocumentCookie_() {
+    return this.win_.document.cookie;
   }
 
   /**
-   * Get the first party cookie for Google Ads
-   * @return {?string}
-   */
-  getGads_() {
-    // TODO(sohanirao): get cookie from cache
-    return null;
-  }
-
-  /**
-   * Returns the client ID to be used
+   * Returns the client ID to be used.
    * @return {?string}
    * @private
    */
   getClientId_() {
-    // No cookie is sent when user consent is not available
+    // No cookie is sent when user consent is not available.
     if (!this.userConsent_) {
       return 'noConsent';
     }
-    // When user consent is available, get Gads cookie
+    // When user consent is available, get the first party cookie
+    // for Google Ads.
     if (!this.clientId_) {
-      this.clientId_ = this.getGads_();
+      // Match '__gads' (name of the cookie) dropped by Ads Tag.
+      const gadsmatch = this.getDocumentCookie_().match(
+          '(^|;)\\s*__gads\\s*=\\s*([^;]+)');
+      // Since the cookie will be consumed using decodeURIComponent(),
+      // use encodeURIComponent() here to match.
+      this.clientId_ = gadsmatch && encodeURIComponent(gadsmatch.pop());
     }
     return this.clientId_;
   }
@@ -191,19 +85,19 @@ export class PropensityServer {
     const init = /** @type {!../utils/xhr.FetchInitDef} */ ({
       method: 'GET',
       credentials: 'include',
-      headers: {'Accept': 'text/plain, application/json'},
     });
     const clientId = this.getClientId_();
-    let url = this.getUrl_() + '/subopt/data?states=' + this.publicationId_
-        + ':' + state;
+    let userState = this.publicationId_ + ':' + state;
     if (entitlements) {
-      url = url + ':' + entitlements;
+      userState = userState + ':' + encodeURIComponent(entitlements);
     }
+    let url = ServiceUrl.adsUrl('/subopt/data?states=')
+        + encodeURIComponent(userState);
     if (clientId) {
       url = url + '&cookie=' + clientId;
     }
     url = url + '&u_tz=240';
-    return this.xhrInterface_.sendRequest(url, init);
+    return this.xhr_.fetch(url, init);
   }
 
   /**
@@ -214,19 +108,19 @@ export class PropensityServer {
     const init = /** @type {!../utils/xhr.FetchInitDef} */ ({
       method: 'GET',
       credentials: 'include',
-      headers: {'Accept': 'text/plain, application/json'},
     });
     const clientId = this.getClientId_();
-    let url = this.getUrl_() + '/subopt/data?events=' + this.publicationId_
-        + ':' + event;
+    let eventInfo = this.publicationId_ + ':' + event;
     if (context) {
-      url = url + ':' + JSON.stringify(context);
+      eventInfo = eventInfo + ':' + encodeURIComponent(context);
     }
+    let url = ServiceUrl.adsUrl('/subopt/data?events=')
+        + encodeURIComponent(eventInfo);
     if (clientId) {
       url = url + '&cookie=' + clientId;
     }
     url = url + '&u_tz=240';
-    return this.xhrInterface_.sendRequest(url, init);
+    return this.xhr_.fetch(url, init);
   }
 
   /**
@@ -235,24 +129,23 @@ export class PropensityServer {
    * @return {?Promise<JsonObject>}
    */
   getPropensity(referrer, type) {
+    const clientId = this.getClientId_();
     const init = /** @type {!../utils/xhr.FetchInitDef} */ ({
       method: 'GET',
       credentials: 'include',
-      headers: {'Accept': 'text/plain, application/json'},
     });
-    const clientId = this.getClientId_();
-    let url = this.getUrl_() + '/subopt/pts?products=' + this.publicationId_
+    let url = ServiceUrl.adsUrl('/subopt/pts?products=') + this.publicationId_
         + '&type=' + type + '&u_tz=240'
         + '&ref=' + referrer;
     if (clientId) {
       url = url + '&cookie=' + clientId;
     }
-    return this.xhrInterface_.sendRequest(url, init).then(response => {
-      let score = response && response.responseText;
-      if (!score) {
-        score = "{'values': [-1]}";
-      }
-      return parseJson(score);
-    });
+    return this.xhr_.fetch(url, init).then(result => result.json())
+        .then(score => {
+          if (!score) {
+            score = "{'values': [-1]}";
+          }
+          return score;
+        });
   }
 }
