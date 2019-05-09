248b760f489a0e3702188fdea0d1157e4da55f61
Matt Borofsky <mborof@google.com>
2019-05-09 14:37:28 -0700

    add more indenting

--- src/proto/api_messages_test.js
+++ src/proto/api_messages_test.js
@@ -29,96 +29,95 @@ import {
 * @return {boolean}
 */
 function isEqual(thisArray, otherArray) {
-if (!otherArray || !thisArray) {
-  return false;
-}
-for (let i = 0; i < otherArray.length; i++) {
-  if (Array.isArray(thisArray[i])) {
-    if (!Array.isArray(otherArray[i])) {
-      return false;
-    }
-    const arr = thisArray[i];
-    const otherArr = otherArray[i];
-    if (arr.length != otherArr.length) {
-      return false;
-    }
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] != otherArr[j]) {
+  if (!otherArray || !thisArray) {
+    return false;
+  }
+  for (let i = 0; i < otherArray.length; i++) {
+    if (Array.isArray(thisArray[i])) {
+      if (!Array.isArray(otherArray[i])) {
+        return false;
+      }
+      const arr = thisArray[i];
+      const otherArr = otherArray[i];
+      if (arr.length != otherArr.length) {
+        return false;
+      }
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] != otherArr[j]) {
+          return false;
+        }
+      }
+    } else {
+      if (thisArray[i] != otherArray[i]) {
         return false;
       }
     }
-  } else {
-    if (thisArray[i] != otherArray[i]) {
-      return false;
-    }
   }
-}
-return true;
+  return true;
 }
 
 
 describe('api_messages', () => {
-
-describe('test_AnalyticsContext', () => {
-  it('should deserialize correctly', () => {
-    const /** !AnalyticsContext  */ analyticscontext = new AnalyticsContext();
-    analyticscontext.setEmbedderOrigin('');
-    analyticscontext.setTransactionId('');
-    analyticscontext.setReferringOrigin('');
-    analyticscontext.setUtmSource('');
-    analyticscontext.setUtmCampaign('');
-    analyticscontext.setUtmMedium('');
-    analyticscontext.setSku('');
-    analyticscontext.setReadyToPay(false);
-    analyticscontext.setLabelList([]);
-    const analyticscontextSerialized = analyticscontext.toArray();
-    const analyticscontextDeserialized = deserialize(
-        analyticscontextSerialized);
-    expect(analyticscontextDeserialized).to.not.be.null;
-    expect(isEqual(analyticscontext.toArray(),
-        analyticscontextDeserialized.toArray())).to.be.true;
+  describe('test_AnalyticsContext', () => {
+    it('should deserialize correctly', () => {
+      const /** !AnalyticsContext  */ analyticscontext = new AnalyticsContext();
+      analyticscontext.setEmbedderOrigin('');
+      analyticscontext.setTransactionId('');
+      analyticscontext.setReferringOrigin('');
+      analyticscontext.setUtmSource('');
+      analyticscontext.setUtmCampaign('');
+      analyticscontext.setUtmMedium('');
+      analyticscontext.setSku('');
+      analyticscontext.setReadyToPay(false);
+      analyticscontext.setLabelList([]);
+      const analyticscontextSerialized = analyticscontext.toArray();
+      const analyticscontextDeserialized = deserialize(
+          analyticscontextSerialized);
+      expect(analyticscontextDeserialized).to.not.be.null;
+      expect(isEqual(analyticscontext.toArray(),
+          analyticscontextDeserialized.toArray())).to.be.true;
+    });
   });
-});
 
-describe('test_AnalyticsEventMeta', () => {
-  it('should deserialize correctly', () => {
-    const /** !AnalyticsEventMeta  */ analyticseventmeta = new AnalyticsEventMeta();
-    analyticseventmeta.setEventOriginator(EventOriginator.UNKNOWN_CLIENT);
-    analyticseventmeta.setIsFromUserAction(false);
-    const analyticseventmetaSerialized = analyticseventmeta.toArray();
-    const analyticseventmetaDeserialized = deserialize(
-        analyticseventmetaSerialized);
-    expect(analyticseventmetaDeserialized).to.not.be.null;
-    expect(isEqual(analyticseventmeta.toArray(),
-        analyticseventmetaDeserialized.toArray())).to.be.true;
+  describe('test_AnalyticsEventMeta', () => {
+    it('should deserialize correctly', () => {
+      const /** !AnalyticsEventMeta  */ analyticseventmeta = new AnalyticsEventMeta();
+      analyticseventmeta.setEventOriginator(EventOriginator.UNKNOWN_CLIENT);
+      analyticseventmeta.setIsFromUserAction(false);
+      const analyticseventmetaSerialized = analyticseventmeta.toArray();
+      const analyticseventmetaDeserialized = deserialize(
+          analyticseventmetaSerialized);
+      expect(analyticseventmetaDeserialized).to.not.be.null;
+      expect(isEqual(analyticseventmeta.toArray(),
+          analyticseventmetaDeserialized.toArray())).to.be.true;
+    });
   });
-});
 
-describe('test_AnalyticsRequest', () => {
-  it('should deserialize correctly', () => {
-    const /** !AnalyticsRequest  */ analyticsrequest = new AnalyticsRequest();
-    const /** !AnalyticsContext  */ analyticscontext = new AnalyticsContext();
-    analyticscontext.setEmbedderOrigin('');
-    analyticscontext.setTransactionId('');
-    analyticscontext.setReferringOrigin('');
-    analyticscontext.setUtmSource('');
-    analyticscontext.setUtmCampaign('');
-    analyticscontext.setUtmMedium('');
-    analyticscontext.setSku('');
-    analyticscontext.setReadyToPay(false);
-    analyticscontext.setLabelList([]);
-    analyticsrequest.setContext(analyticscontext);
-    analyticsrequest.setEvent(AnalyticsEvent.UNKNOWN);
-    const /** !AnalyticsEventMeta  */ analyticseventmeta = new AnalyticsEventMeta();
-    analyticseventmeta.setEventOriginator(EventOriginator.UNKNOWN_CLIENT);
-    analyticseventmeta.setIsFromUserAction(false);
-    analyticsrequest.setMeta(analyticseventmeta);
-    const analyticsrequestSerialized = analyticsrequest.toArray();
-    const analyticsrequestDeserialized = deserialize(
-        analyticsrequestSerialized);
-    expect(analyticsrequestDeserialized).to.not.be.null;
-    expect(isEqual(analyticsrequest.toArray(),
-        analyticsrequestDeserialized.toArray())).to.be.true;
+  describe('test_AnalyticsRequest', () => {
+    it('should deserialize correctly', () => {
+      const /** !AnalyticsRequest  */ analyticsrequest = new AnalyticsRequest();
+      const /** !AnalyticsContext  */ analyticscontext = new AnalyticsContext();
+      analyticscontext.setEmbedderOrigin('');
+      analyticscontext.setTransactionId('');
+      analyticscontext.setReferringOrigin('');
+      analyticscontext.setUtmSource('');
+      analyticscontext.setUtmCampaign('');
+      analyticscontext.setUtmMedium('');
+      analyticscontext.setSku('');
+      analyticscontext.setReadyToPay(false);
+      analyticscontext.setLabelList([]);
+      analyticsrequest.setContext(analyticscontext);
+      analyticsrequest.setEvent(AnalyticsEvent.UNKNOWN);
+      const /** !AnalyticsEventMeta  */ analyticseventmeta = new AnalyticsEventMeta();
+      analyticseventmeta.setEventOriginator(EventOriginator.UNKNOWN_CLIENT);
+      analyticseventmeta.setIsFromUserAction(false);
+      analyticsrequest.setMeta(analyticseventmeta);
+      const analyticsrequestSerialized = analyticsrequest.toArray();
+      const analyticsrequestDeserialized = deserialize(
+          analyticsrequestSerialized);
+      expect(analyticsrequestDeserialized).to.not.be.null;
+      expect(isEqual(analyticsrequest.toArray(),
+          analyticsrequestDeserialized.toArray())).to.be.true;
+    });
   });
-});
 });
