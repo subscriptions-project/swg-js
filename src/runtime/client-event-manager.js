ee141db822b444933cab255c2430db94edd47ead
Matt Borofsky <mborof@google.com>
2019-05-16 13:19:27 -0700

    add alll event man code

--- src/runtime/client-event-manager.js
+++ src/runtime/client-event-manager.js
@@ -14,41 +14,101 @@
  * limitations under the License.
  */
 
-import * as EventApi from '../api/client-event-manager-api';
+import {AnalyticsEvent,EventOriginator} from '../proto/api_messages';
+import {isObject, isFunction, isEnumValue, isBoolean} from '../utils/types';
+import * as EventManagerApi from '../api/swg-client-event-manager-api';
+import { isUndefined } from 'util';
 
-/** @implements {../api/client-event-manager-api.ClientEventManagerApi}*/
-export class ClientEventManager {
+/**
+ * Helper function to describe an issue with an event object
+ * @param {!string} valueName
+ * @param {?*} value
+ * @returns {!string}
+ */
+function createEventErrorMessage(valueName, value) {
+  return 'Event has an invalid ' + valueName + '(' + value + ')';
+}
+
+/**
+ * Throws an error if the event is invalid.
+ * @param {!EventManagerApi.SwgClientEvent} event
+ * @returns {!Promise}
+ */
+function validateEvent(event) {
+  if (!isObject(event)) {
+    throw new Error('Event must be a valid object');
+  }
+
+  if (!isEnumValue(AnalyticsEvent, event.eventType)) {
+    throw new Error(createEventErrorMessage('eventType', event.eventType));
+  }
+  if (!isEnumValue(EventOriginator, event.eventOriginator)) {
+    throw new Error(createEventErrorMessage('eventOriginator',
+        event.eventOriginator));
+  }
+  if (!isObject(event.additionalParameters)
+      && event.additionalParameters !== null) {
+    if (!isUndefined(event.additionalParameters)) {
+      throw new Error(createEventErrorMessage('additionalParameters',
+          event.additionalParameters));
+    }
+    event.additionalParameters = null;
+  }
+  if (event.isFromUserAction !== null && !isBoolean(event.isFromUserAction)) {
+    throw new Error(createEventErrorMessage('isFromUserAction',
+        event.isFromUserAction));
+  }
+  return Promise.resolve();
+}
+
+/** @implements {EventManagerApi.SwgClientEventManagerApi} */
+export class SwgClientEventManager {
   constructor() {
-    /** @private {Array<function(EventApi.SwgClientEvent)>} */
+    /** @private {!Array<function(!EventManagerApi.SwgClientEvent)>} */
     this.listeners_ = [];
 
-    /** @private {Array<function(EventApi.SwgClientEvent):EventApi.FilterResult>} */
+    /** @private {!Array<function(!EventManagerApi.SwgClientEvent):!EventManagerApi.FilterResult>} */
     this.filterers_ = [];
   }
 
   /**
    * @overrides
    */
-  registerEventListener(listener) {
-    this.listeners_.push(listener);
+  registerEventListener(callback) {
+    if (!isFunction(callback)) {
+      throw new Error('Event manager listeners must be a function');
+    }
+    this.listeners_.push(callback);
   }
 
   /**
    * @overrides
    */
-  registerEventFilterer(filterer) {
-    this.filterers_.push(filterer);
+  registerEventFilterer(callback) {
+    if (!isFunction(callback)) {
+      throw new Error('Event manager filterers must be a function');
+    }
+    this.filterers_.push(callback);
   }
 
   /**
    * @overrides
    */
   logEvent(event) {
-    //TODO(mborof): the API must be used somewhere or presubmit fails.
-    //              Remove this line once all code is implemented
-    if (event === FilterResult.CANCEL_EVENT) {
-      return;
-    }
-    return;
+    /** @private {Promise} */
+    this.lastAction_ = validateEvent(event).then(() => {
+      let callbackNum;
+      for (callbackNum = 0; callbackNum < this.filterers_.length; callbackNum++)
+      {
+        if (this.filterers_[callbackNum](event)
+            === EventManagerApi.FilterResult.STOP_EXECUTING) {
+          return;
+        }
+      }
+      for (callbackNum = 0; callbackNum < this.listeners_.length; callbackNum++)
+      {
+        this.listeners_[callbackNum](event);
+      }
+    });
   }
 }
