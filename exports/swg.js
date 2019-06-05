6b1081c05837119b44d686ccdaf25d61ddff0031
Matt Borofsky <mborof@google.com>
2019-06-05 15:47:03 -0700

    export enums for amp

--- exports/swg.js
+++ exports/swg.js
@@ -23,7 +23,7 @@ import {Fetcher} from '../src/runtime/fetcher';
 import {SubscribeResponse} from '../src/api/subscribe-response';
 import {ClientEventManagerApi,ClientEvent,FilterResult}
     from '../src/api/client-event-manager-api';
-
+import {AnalyticsEvent,EventOriginator} from '../src/proto/api_messages';
 
 module.exports = {
   ConfiguredRuntime,
@@ -34,4 +34,6 @@ module.exports = {
   ClientEventManagerApi,
   ClientEvent,
   FilterResult,
+  AnalyticsEvent,
+  EventOriginator,
 };
