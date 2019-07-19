25307262239f3fd05d29bf7ba5341c758b6dce1c
Matt Borofsky <mborof@google.com>
2019-07-19 11:16:44 -0700

    remove Event and SubscriptionState from propen-api

--- src/runtime/propensity-test.js
+++ src/runtime/propensity-test.js
@@ -15,6 +15,7 @@
  */
 import {Propensity} from './propensity';
 import * as PropensityApi from '../api/propensity-api';
+import {SubscriptionState,Event} from '../api/logger-api';
 import {PageConfig} from '../model/page-config';
 import {PropensityServer} from './propensity-server';
 import {ClientEventManager} from './client-event-manager';
@@ -36,7 +37,7 @@ describes.realWin('Propensity', {}, env => {
     sandbox.stub(PropensityServer.prototype, 'sendSubscriptionState', () => {});
 
     expect(() => {
-      propensity.sendSubscriptionState(PropensityApi.SubscriptionState.UNKNOWN);
+      propensity.sendSubscriptionState(SubscriptionState.UNKNOWN);
     }).to.not.throw('Invalid subscription state provided');
     expect(() => {
       propensity.sendSubscriptionState('past');
@@ -49,32 +50,32 @@ describes.realWin('Propensity', {}, env => {
 
     expect(() => {
       propensity.sendSubscriptionState(
-          PropensityApi.SubscriptionState.SUBSCRIBER);
+          SubscriptionState.SUBSCRIBER);
     }).to.throw('Entitlements must be provided for users with'
         + ' active or expired subscriptions');
     expect(() => {
       propensity.sendSubscriptionState(
-          PropensityApi.SubscriptionState.PAST_SUBSCRIBER);
+          SubscriptionState.PAST_SUBSCRIBER);
     }).to.throw('Entitlements must be provided for users with'
         + ' active or expired subscriptions');
     expect(() => {
       const productsOrSkus = {};
       productsOrSkus['product'] = 'basic-monthly';
       propensity.sendSubscriptionState(
-          PropensityApi.SubscriptionState.SUBSCRIBER, productsOrSkus);
+          SubscriptionState.SUBSCRIBER, productsOrSkus);
     }).not.throw('Entitlements must be provided for users with'
         + ' active or expired subscriptions');
     expect(() => {
       const productsOrSkus = {};
       productsOrSkus['product'] = 'basic-monthly';
       propensity.sendSubscriptionState(
-          PropensityApi.SubscriptionState.PAST_SUBSCRIBER, productsOrSkus);
+          SubscriptionState.PAST_SUBSCRIBER, productsOrSkus);
     }).not.throw('Entitlements must be provided for users with'
         + ' active or expired subscriptions');
     expect(() => {
       const productsOrSkus = ['basic-monthly'];
       propensity.sendSubscriptionState(
-          PropensityApi.SubscriptionState.SUBSCRIBER, productsOrSkus);
+          SubscriptionState.SUBSCRIBER, productsOrSkus);
     }).throw(/Entitlements must be an Object/);
   });
 
@@ -97,9 +98,9 @@ describes.realWin('Propensity', {}, env => {
           subscriptionState = state;
         });
     expect(() => {
-      propensity.sendSubscriptionState(PropensityApi.SubscriptionState.UNKNOWN);
+      propensity.sendSubscriptionState(SubscriptionState.UNKNOWN);
     }).to.not.throw('Invalid subscription state provided');
-    expect(subscriptionState).to.equal(PropensityApi.SubscriptionState.UNKNOWN);
+    expect(subscriptionState).to.equal(SubscriptionState.UNKNOWN);
   });
 
   it('should report server errors', () => {
@@ -107,7 +108,7 @@ describes.realWin('Propensity', {}, env => {
       throw new Error('publisher not whitelisted');
     });
     expect(() => {
-      propensity.sendSubscriptionState(PropensityApi.SubscriptionState.UNKNOWN);
+      propensity.sendSubscriptionState(SubscriptionState.UNKNOWN);
     }).to.throw('publisher not whitelisted');
   });
 
@@ -116,7 +117,7 @@ describes.realWin('Propensity', {}, env => {
     sandbox.stub(ClientEventManager.prototype, 'logEvent',
         event => eventSent = event);
     propensity.sendEvent({
-      name: PropensityApi.Event.IMPRESSION_PAYWALL,
+      name: Event.IMPRESSION_PAYWALL,
     });
     expect(eventSent).to.deep.equal({
       eventType: AnalyticsEvent.IMPRESSION_PAYWALL,
@@ -154,7 +155,7 @@ describes.realWin('Propensity', {}, env => {
     //ensure it takes a valid enum with nothing else and fills in appropriate
     //defaults for other values
     testSend({
-      name: PropensityApi.Event.IMPRESSION_PAYWALL,
+      name: Event.IMPRESSION_PAYWALL,
     });
     expect(hasError).to.be.false;
     expect(receivedEvent).to.deep.equal({
@@ -166,7 +167,7 @@ describes.realWin('Propensity', {}, env => {
 
     //ensure it respects the active flag
     testSend({
-      name: PropensityApi.Event.IMPRESSION_OFFERS,
+      name: Event.IMPRESSION_OFFERS,
     });
     expect(receivedEvent).to.deep.equal({
       eventType: AnalyticsEvent.IMPRESSION_OFFERS,
@@ -176,7 +177,7 @@ describes.realWin('Propensity', {}, env => {
     });
 
     testSend({
-      name: PropensityApi.Event.IMPRESSION_OFFERS,
+      name: Event.IMPRESSION_OFFERS,
       active: null,
     });
     expect(hasError).to.be.false;
@@ -188,7 +189,7 @@ describes.realWin('Propensity', {}, env => {
     });
 
     testSend({
-      name: PropensityApi.Event.IMPRESSION_OFFERS,
+      name: Event.IMPRESSION_OFFERS,
       active: true,
     });
     expect(hasError).to.be.false;
@@ -200,7 +201,7 @@ describes.realWin('Propensity', {}, env => {
     });
 
     testSend({
-      name: PropensityApi.Event.IMPRESSION_OFFERS,
+      name: Event.IMPRESSION_OFFERS,
       active: false,
     });
     expect(hasError).to.be.false;
@@ -213,7 +214,7 @@ describes.realWin('Propensity', {}, env => {
 
     //ensure it rejects invalid data objects
     testSend({
-      name: PropensityApi.Event.IMPRESSION_OFFERS,
+      name: Event.IMPRESSION_OFFERS,
       active: true,
       data: 'all_offers',
     });
