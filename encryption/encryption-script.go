ae87ca1c3b11320dda1c302c0eb756cc64f0d0fc
Elijah Soria <elijahsoria@google.com>
2019-10-07 08:40:29 -0700

    Debugging but it is working well now.

--- encryption/encryption-script.go
+++ encryption/encryption-script.go
@@ -11,11 +11,12 @@ import (
 func main() {
 	html_file := flag.String("input_html_file", "", "Input HTML file to encrypt.")
 	out_file := flag.String("output_file", "", "Output path to write encrypted HTML file.")
+	access_requirement := flag.String("access_requirement", "", "The access requirement we grant upon decryption.")
 	google_public_key_url := flag.String("google_public_key_url",
 			"https://news.google.com/swg/encryption/keys/dev/tink/public_key",
 			"URL to Google's public key.")
 	flag.Parse()
-	if *html_file == "" || *out_file == "" || *google_public_key_url == "" {
+	if *html_file == "" || *out_file == "" || *google_public_key_url == "" || *access_requirement == "" {
 		fmt.Println("Missing flags!")
 		os.Exit(42)
 	}
@@ -24,7 +25,7 @@ func main() {
 		fmt.Println(err)
 		os.Exit(42)
 	}
-	encrypted_doc, err := encryptionutils.GenerateEncryptedDocument(string(b), *google_public_key_url)
+	encrypted_doc, err := encryptionutils.GenerateEncryptedDocument(string(b), *google_public_key_url, *access_requirement)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(42)
