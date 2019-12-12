9319bcd51dd063e0b03fac56829338055dcf8483
Matt Borofsky <mborof@google.com>
2019-12-12 09:30:19 -0800

    Make it easier to get an SKU from entitlements (#853)
    * Make it easier to get an SKU from an entitlement.
    * Send current entitlement to analytics server.

--- src/runtime/runtime.js
+++ src/runtime/runtime.js
@@ -729,7 +729,20 @@ export class ConfiguredRuntime {
   getEntitlements(encryptedDocumentKey) {
     return this.entitlementsManager_
       .getEntitlements(encryptedDocumentKey)
-      .then(entitlements => entitlements.clone());
+      .then(entitlements => {
+        // Auto update internal things tracking the user's current SKU.
+        if (entitlements) {
+          try {
+            const skus = entitlements.entitlements.map(
+              entitlement => entitlement.getSku() || 'unknown subscriptionToken'
+            );
+            if (skus.length > 0) {
+              this.analyticsService_.setSku(skus.join(','));
+            }
+          } catch (ex) {}
+        }
+        return entitlements.clone();
+      });
   }
 
   /** @override */
