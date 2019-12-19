042766bd8eabc4723e2db531b4da305ba8e220ba
Matt Borofsky <mborof@google.com>
2019-12-19 10:39:35 -0800

    remove debug statements

--- src/runtime/callbacks.js
+++ src/runtime/callbacks.js
@@ -177,14 +177,12 @@ export class Callbacks {
   triggerPaymentResponse(responsePromise) {
     this.paymentResponsePromise_ = responsePromise.then(
       res => {
-        debugger;
         this.trigger_(
           CallbackId.PAYMENT_RESPONSE,
           Promise.resolve(res.clone())
         );
       },
       reason => {
-        debugger;
         if (isCancelError(reason)) {
           return;
         }
@@ -264,7 +262,6 @@ export class Callbacks {
    * @private
    */
   trigger_(id, data) {
-    debugger;
     this.resultBuffer_[id] = data;
     const callback = this.callbacks_[id];
     if (callback) {
