79ce4282ba54485d012c63c6261187c856c135b4
Matt Borofsky <mborof@google.com>
2020-07-31 17:20:06 -0700

    fix promise compile

--- src/polyfills/promise.js
+++ src/polyfills/promise.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import * as Promise from 'promise-pjs/promise';
+import {default as def} from 'promise';
 
 /**
  * Sets the Promise polyfill if it does not exist.
@@ -25,8 +25,8 @@ export function install(win) {
     win.Promise = /** @type {?} */ (Promise);
     // In babel the * export is an Object with a default property.
     // In closure compiler it is the Promise function itself.
-    if (Promise.default) {
-      win.Promise = Promise.default;
+    if (def) {
+      win.Promise = def;
     }
     // We copy the individual static methods, because closure
     // compiler flattens the polyfill namespace.
