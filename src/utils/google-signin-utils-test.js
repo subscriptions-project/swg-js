894df1787db3ef6c2550b79a7103a5c0b84733fb
Elijah Soria <elijahsoria@google.com>
2020-09-08 08:28:47 -0700

    Adding files

---
+++ src/utils/google-signin-utils-test.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  ENTITLEMENTS_READY_COMMAND,
+  PARENT_READY_COMMAND,
+  SENTINEL,
+  SwgGoogleSigninCreator,
+  createGoogleSigninCallback,
+} from './google-signin-utils';
+
+describes.realWin('SwgGoogleSigninCreator', {}, (env) => {
+  let win;
+  let allowedOrigins;
+  let signinCallback;
+  let parentMock;
+
+  const testMeteringObject = {
+    metering: {
+      state: {
+        id: 'user5901e3f7a7fc5767b6acbbbaa927d36f5901e3f7a7fc5767b6acbbbaa927',
+        standardAttributes: {
+          registeredUser: {
+            timestamp: 10000000,
+          },
+        },
+        customAttributes: {
+          newsletterSubscriber: {
+            timestamp: 10000000,
+          },
+        },
+      },
+    },
+  };
+
+  function testCallback() {
+    return JSON.stringify(testMeteringObject);
+  }
+
+  beforeEach(() => {
+    win = env.win;
+    allowedOrigins = ['localhost'];
+    signinCallback = sandbox.spy();
+    parentMock = sandbox.mock(win.parent);
+  });
+
+  afterEach(() => {
+    parentMock.verify();
+  });
+
+  describe('createGoogleSigninCallback', () => {
+    it('should add response handling to input callback', () => {
+      const modifiedCallback = createGoogleSigninCallback(win, testCallback);
+      parentMock.expects('postMessage').once().withExactArgs(
+        {
+          sentinel: SENTINEL,
+          command: ENTITLEMENTS_READY_COMMAND,
+          response: testMeteringObject,
+        },
+        '*'
+      );
+      modifiedCallback();
+    });
+  });
+
+  describe('start', () => {
+    it('should start correctly and accept events', () => {
+      const creator = new SwgGoogleSigninCreator(
+        allowedOrigins,
+        signinCallback,
+        win
+      );
+      parentMock.expects('postMessage').once();
+      creator.start();
+      win.postMessage(
+        {
+          sentinel: SENTINEL,
+          command: PARENT_READY_COMMAND,
+          nonce: creator.pendingNonce_,
+        },
+        '*'
+      );
+    });
+  });
+});
