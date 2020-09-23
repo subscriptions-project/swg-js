76781bd1edd67077806ab3cd4cbca7bfdab6f45c
Elijah Soria <elijahsoria@google.com>
2020-09-23 11:35:27 -0700

    Change from show offers to alert in demo

--- examples/sample-pub/publisher.js
+++ examples/sample-pub/publisher.js
@@ -167,8 +167,7 @@ function startFlowAuto() {
       // Set native response to a subscribe request. Required for metering toast.
       subscriptions.setOnNativeSubscribeRequest(() => {
         console.log('Starting native subscribe flow');
-        // Show offers for demo purposes.
-        startFlow('showOffers');
+        alert('Subscribe to The Scenic today!');
       });
 
       // Handles metering entitlements.
