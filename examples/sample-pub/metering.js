7ce566c9be8ea32c5cb0ab5b886589fb3ba80759
Chris Antaki <cfa@google.com>
2020-09-24 14:34:03 -0700

    Define origin for demo iframe URL

--- examples/sample-pub/metering.js
+++ examples/sample-pub/metering.js
@@ -9,6 +9,10 @@ const MeteringDemo = {
   GOOGLE_SIGN_IN_CLIENT_ID:
     '520465458218-e9vp957krfk2r0i4ejeh6aklqm7c25p4.apps.googleusercontent.com',
 
+  /** URL of iframe containing a Google Sign-In button. */
+  GOOGLE_SIGN_IN_IFRAME_URL:
+    'http://localhost:8000/examples/sample-pub/gsi-iframe',
+
   /** Sets up controls for the metering demo. */
   setupControls: () => {
     // Wire up buttons.
