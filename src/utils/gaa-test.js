01c97b57bec1876d22df5215e0c1d3c3545e7845
Chris Antaki <cfa@google.com>
2020-12-03 17:27:08 -0800

    Regwall: Only render if URL has fresh GAA URL params (#1390)
    * Regwall: Check for fresh GAA params
    
    * Fixes wording
    
    * Removes extra var
    
    * Fixes Closure issue
    
    * Clarity
    
    * Simplify

--- src/utils/gaa-test.js
+++ src/utils/gaa-test.js
@@ -27,7 +27,6 @@ import {
 import {I18N_STRINGS} from '../i18n/strings';
 import {tick} from '../../test/tick';
 
-const ARTICLE_URL = '/article';
 const PUBLISHER_NAME = 'The Scenic';
 const IFRAME_URL = 'https://localhost/gsi-iframe';
 
@@ -98,8 +97,11 @@ describes.realWin('GaaMeteringRegwall', {}, () => {
 
     // Mock location.
     GaaMeteringRegwall.location_ = {
-      href: ARTICLE_URL,
+      search: '?gaa_at=gaa&gaa_n=n0nc3&gaa_sig=s1gn4tur3&gaa_ts=99999999',
     };
+
+    // Mock console.warn method.
+    sandbox.stub(self.console, 'warn');
 
     // Add JSON-LD with a publisher name.
     script = self.document.createElement('script');
@@ -112,6 +114,7 @@ describes.realWin('GaaMeteringRegwall', {}, () => {
     script.remove();
     GaaMeteringRegwall.remove_();
     self.document.documentElement.lang = '';
+    self.console.warn.restore();
   });
 
   describe('show', () => {
@@ -186,6 +189,32 @@ describes.realWin('GaaMeteringRegwall', {}, () => {
       );
       expect(titleEl.textContent).to.equal(
         I18N_STRINGS.GAA_REGWALL_TITLE['pt-br']
+      );
+    });
+
+    it('fails if GAA URL params are missing', () => {
+      // Remove GAA URL params.
+      GaaMeteringRegwall.location_.search = '';
+
+      GaaMeteringRegwall.show({iframeUrl: IFRAME_URL});
+
+      expect(self.console.warn).to.have.been.calledWithExactly(
+        '[swg-gaa.js:GaaMeteringRegwall.show]: URL needs fresh GAA params.'
+      );
+    });
+
+    it('fails if GAA URL params are expired', () => {
+      // Add GAA URL params with expiration of 7 seconds.
+      GaaMeteringRegwall.location_.search =
+        '?gaa_at=gaa&gaa_n=n0nc3&gaa_sig=s1gn4tur3&gaa_ts=7';
+
+      // Move clock a little past 7 seconds.
+      clock.tick(7001);
+
+      GaaMeteringRegwall.show({iframeUrl: IFRAME_URL});
+
+      expect(self.console.warn).to.have.been.calledWithExactly(
+        '[swg-gaa.js:GaaMeteringRegwall.show]: URL needs fresh GAA params.'
       );
     });
   });
