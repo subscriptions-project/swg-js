ca89272779642a3b64e07f51572f4ed88093a12b
Chris Antaki <cfa@google.com>
2021-03-15 09:34:16 -0700

    Simplify type

--- src/utils/xhr.js
+++ src/utils/xhr.js
@@ -371,7 +371,7 @@ export class FetchResponseHeaders {
 
   /**
    * @param {string} name
-   * @return {string|null}
+   * @return {?string}
    */
   get(name) {
     return this.xhr_.getResponseHeader(name);
