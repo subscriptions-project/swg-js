7d1981706863a08844741e7c92070230becb19f7
Chris Antaki <ChrisAntaki@gmail.com>
2021-04-12 12:35:14 -0700

    Brings back method copying.

--- src/polyfills/promise.js
+++ src/polyfills/promise.js
@@ -23,5 +23,16 @@ import Promise from 'promise-pjs/promise';
 export function install(win) {
   if (!win.Promise) {
     win.Promise = /** @type {?} */ (Promise);
+    // In babel the * export is an Object with a default property.
+    // In closure compiler it is the Promise function itself.
+    if (Promise.default) {
+      win.Promise = Promise.default;
+    }
+    // We copy the individual static methods, because closure
+    // compiler flattens the polyfill namespace.
+    win.Promise.resolve = Promise.resolve;
+    win.Promise.reject = Promise.reject;
+    win.Promise.all = Promise.all;
+    win.Promise.race = Promise.race;
   }
 }
