ceb3e7b9bb5808812f6531edc05deac65684599c
Chris Antaki <cfa@google.com>
2021-06-24 17:39:16 -0700

    Follow the rules

--- demos/index.js
+++ demos/index.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021 The Subscribe with Google Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
@@ -5,5 +20,6 @@ const port = process.env.PORT || 8000;
 app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Swgjs Demos are available at http://localhost:${port}`);
+  console /*OK*/
+    .log(`Swgjs Demos are available at http://localhost:${port}`);
 });
