1eb8c8e25d6be20d5dacff31564f1717b2357e4a
Erin Sparling <erin@everyplace.net>
2021-07-27 15:29:12 +0000

    Set alwaysShow to false as a default fallback.

--- src/runtime/basic-runtime.js
+++ src/runtime/basic-runtime.js
@@ -198,7 +198,7 @@ export class BasicRuntime {
     this.clientOptions_ = params.clientOptions;
     this.setupAndShowAutoPrompt({
       autoPromptType: params.autoPromptType,
-      alwaysShow: params.alwaysShow,
+      alwaysShow: params.alwaysShow || false
     });
     this.setOnLoginRequest();
     this.processEntitlements();
