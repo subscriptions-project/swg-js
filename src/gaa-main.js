2e53c5cc2878d6fc135d383d0299479ec04687fe
mark-yoon <markyoon@google.com>
2021-09-30 17:56:33 -0700

    gaa: Add 3p sign in button

--- src/gaa-main.js
+++ src/gaa-main.js
@@ -19,11 +19,16 @@
  * The entry point for Showcase (swg-gaa.js).
  */
 
-import {GaaGoogleSignInButton, GaaMeteringRegwall} from './utils/gaa';
+import {
+  GaaGoogle3pSignInButton,
+  GaaGoogleSignInButton,
+  GaaMeteringRegwall,
+} from './utils/gaa';
 import {log} from './utils/log';
 
 log('Subscriptions Showcase Version: $internalRuntimeVersion$');
 
 // Declare global variables.
 self.GaaGoogleSignInButton = GaaGoogleSignInButton;
+self.GaaGoogle3pSignInButton = GaaGoogle3pSignInButton;
 self.GaaMeteringRegwall = GaaMeteringRegwall;
