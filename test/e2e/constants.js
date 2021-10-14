18c9b9a9ef2109014fbeb888e1940fe0f58386f9
Chris Antaki <cfa@google.com>
2021-10-14 09:42:28 -0700

    Revert "E2E: Mock HTTP response from Google Pay. (#1937)" (#1946)
    This reverts commit 908cd6a29d3359b5322f1a54bf58ad22906d4aa0.

--- test/e2e/constants.js
+++ test/e2e/constants.js
@@ -15,8 +15,8 @@
  */
 
 module.exports = {
-  gpay: {
-    origin: 'https://pay.sandbox.google.com',
+  google: {
+    domain: 'google.com',
   },
   setup: {
     url: 'http://localhost:8000/examples/sample-pub/setup',
