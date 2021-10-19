c6517ee43b260b60c5d24b16d779aba4e0959ee9
mmmicedcoffee <irenechen@google.com>
2021-10-18 17:43:52 -0700

    Clear out padding-bottom set on the document root element for centered dialogs.
    For these dialogs, we aren't concerned about the dialog blocking content on the page.

--- src/components/dialog-test.js
+++ src/components/dialog-test.js
@@ -537,6 +537,19 @@ describes.realWin('Dialog', {}, (env) => {
         .querySelector('swg-loading-container');
       expect(loadingContainer).to.have.class('centered-on-desktop');
     });
+
+    it('adds padding-bottom to document root on resize', async () => {
+      immediate();
+      await dialog.open();
+      await dialog.openView(view);
+
+      const newHeight = 110;
+      await dialog.resizeView(view, newHeight, ANIMATE);
+
+      expect(win.document.documentElement.style.paddingBottom).to.equal(
+        `${newHeight + 20}px`
+      );
+    });
   });
 
   describe('dialog with isCenterPositioned=true on desktop', () => {
@@ -617,6 +630,17 @@ describes.realWin('Dialog', {}, (env) => {
       const iframe = dialog.getElement();
       expectPositionCenter(iframe);
       expect(getStyle(iframe, 'height')).to.equal(`${newHeight}px`);
+    });
+
+    it('does not add padding-bottom to document root on resize', async () => {
+      immediate();
+      await dialog.open();
+      await dialog.openView(view);
+
+      const newHeight = 110;
+      await dialog.resizeView(view, newHeight, ANIMATE);
+
+      expect(win.document.documentElement.style.paddingBottom).to.equal('');
     });
   });
 });
