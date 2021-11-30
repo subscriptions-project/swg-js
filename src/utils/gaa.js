71a9e3365932d78e34156925a52d687ede91f309
rew326 <revian326@gmail.com>
2021-11-30 13:24:19 +0000

    Adding client-side sample code to GaaMetering class

--- src/utils/gaa.js
+++ src/utils/gaa.js
@@ -1231,3 +1231,212 @@ export class GaaUtils {
     return self.location.search;
   }
 }
+
+export class GaaMetering {
+  /**
+   * Returns a promise for a Google user object.
+   * The user object will be a:
+   * - GaaUserDef, if you use the GaaGoogleSignInButton
+   * - GoogleIdentityV1, if you use the GaaSignInWithGoogleButton
+   * - Custom object, if you use the GaaGoogle3pSignInButton
+   *
+   * This method opens a metering regwall dialog,
+   * where users can sign in with Google.
+   * @nocollapse
+   * @param {!GaaMeteringParams} params
+   * WHAT BELONGS HERE? @return {!Promise<!GaaUserDef|!GoogleIdentityV1|!Object>}
+   */
+  static init({params}) {
+    // Publisher-defined promise
+    const registerUserPromise = new Promise(...);
+    const publisherEntitlementPromise = new Promise(...);
+
+    // Publisher-defined functions
+    //function isArticleFree() { ... }
+    //function isUserRegistered() { ... }
+    //function anonymousUserHasAccess() { ... }
+    function unlockArticle() { ... }
+    function showPaywall() { ... }
+    function openLoginPage() { ... }
+    //function isGaa{ ... } // See the isGaa() example
+    // Get userState by calling your userState endpoint server or
+    // retrieving from server-side HTTP only cookie
+    function getUserState = { ... };
+
+    function syncEntitlementsWithGoogle() {
+     return checkShowcaseEntitlement(getUserState());
+    }
+
+
+    // Setting up swg.js and registering callbacks
+    (self.SWG = self.SWG || []).push(function(subscriptions) {
+      // Use a full productID (publication ID and product label).
+      subscriptions.init("examplenews.com:showcase");
+
+      // Define function to send userState to Google using
+      // subscriptions object
+      function checkShowcaseEntitlement(userState) {
+        if (userState) {
+          // Send userState to Google
+          subscriptions.getEntitlements(userState);
+        } else {
+          // If userState is undefined, it’s likely the user isn’t
+          // logged in. Do not send an empty userState to Google in
+          // this case.
+          showGoogleRegwall();
+        }
+      }
+
+     // Show the Google registration intervention.
+     function showGoogleRegwall() {
+       GaaMeteringRegwall.show({
+         // Specify a URL that renders a Google Sign-In button.
+         // See the Google Sign-In widget section.
+         iframeUrl: "https://examplenews.com/registration_widget.html",
+       }).then((googleSignInDetails) => {
+         // A.5biii) Handle registration for new users
+         // Send the googleSignInDetails object to your Registration endpoint.
+         // Return a userState object to represent the
+         // newly-registered user.
+         registerUserPromise(googleSignInDetails).then((userState) => {
+             // Send the userState object for the newly-registered
+             // user to Google.
+             checkShowcaseEntitlement(userState);
+           });
+       });
+     }
+
+
+     // Step A: Handle anonymous Showcase users.
+     // A.1) Check if the user came from showcase
+     if (isGaa()) {
+       // A.2) Check if the article is free
+       if (isArticleFree()) {
+         subscriptions.setShowcaseEntitlement({
+           entitlement: 'EVENT_SHOWCASE_UNLOCKED_FREE_PAGE',
+           isUserRegistered: isUserRegistered()
+         });
+       // A.3) Check if the publisher knows the user or not.
+       } else if (isUserRegistered()) {
+         // A.4) The publisher knows who the user is already.
+         // Create a userState object for the user and send it to Google
+         syncEntitlementsWithGoogle();
+       // A.5) The user is anonymous and is trying to read a paywalled article.
+       } else {
+         // If the publisher has a meter for anonymous users, the publisher should check
+         // if the user has reads left from that meter.
+         if (anonymousUserHasAccess()) {
+           // A.5a) The user has access from the anonymous user meter
+           subscriptions.setShowcaseEntitlement({
+             entitlement: 'EVENT_SHOWCASE_UNLOCKED_BY_METER',
+             isUserRegistered: false
+           });
+           unlockArticle();
+         } else {
+           // A.5b) The publisher does not have a meter for anonymous users or the current
+           // user has run out of reads.
+
+           // A.5bi) Show the registration intervention
+           showGoogleRegwall();
+           // A.5bii) Handle login for existing users
+           subscriptions.setOnLoginRequest(() => {
+             // Open the login page
+             openLoginPage();
+             // Once the user has logged in, send the userState object to Google
+             // See the Sync entitlements with Google section.
+           });
+         }
+       }
+     } else {
+       // Carry out normal paywall decision
+     }
+
+     // Step B: Sync entitlements with Google.
+     // Define the callback that is triggered once the entitlement
+     // response is returned from Google. This is the main logic to
+     // determine what to show to users depending on:
+     // - If the already have access from the publisher
+     // - If Google is granting extended access to a registered user
+     // - If the user is not yet registered
+     subscriptions.setOnEntitlementsResponse((entitlementsPromise) => {
+       // Wait for Google check and publisher check to finish
+       Promise.all([entitlementsPromise, publisherAccessCheck]).then((entitlements) => {
+         // Determine Google response from publisher response.
+         const [googleEntitlement, publisherEntitlement] = entitlements;
+
+         if(publisherEntitlement.granted) {
+           // B.1b) User has access from publisher so unlock article
+           unlockArticle();
+           // At the same time, share information about the entitlement with Google.
+           // See here for the specification of PublisherEntitlement: https://git.io/Jk1SW
+           if(publisherEntitlement.grantReason === 'SUBSCRIBER') {
+             // B.1ai) The user has access because they have a subscription
+             subscriptions.setShowcaseEntitlement({
+               entitlement: 'EVENT_SHOWCASE_UNLOCKED_BY_SUBSCRIPTION',
+               isUserRegistered: isUserRegistered()
+             });
+           } else if (publisherEntitlement.grantReason === 'METERED') {
+             // B.1aii) The user has access from the publisher's meter
+             subscriptions.setShowcaseEntitlement({
+               entitlement: 'EVENT_SHOWCASE_UNLOCKED_BY_METER',
+               isUserRegistered: isUserRegistered()
+             });
+           }
+       } else if (googleEntitlement.enablesThisWithGoogleMetering()) {
+        // B.2a) Google returned metering entitlement so grant access
+        googleEntitlement.consume(() => {
+          // B.2ai) Consume the entitlement and trigger a dialog that lets the user
+          // know Google provided them with a free read.
+
+          // B.2aii) Unlock the article AFTER the user consumes a free read.
+          unlockArticle();
+        });
+       } else if (googleEntitlement.enablesThis()) {
+         // B.2b) Google returned a non-metering entitlement
+         // This is only relevant for publishers doing SwG
+         handleSwGEntitlement();
+       } else if (!isUserRegistered() && isGaa()) {
+         // This is an anonymous user so show the Google registration intervention
+         showGoogleRegwall();
+       } else {
+         // B.3a) User does not any access from publisher or Google so show the standard paywall
+         subscriptions.setShowcaseEntitlement({
+           entitlement: 'EVENT_SHOWCASE_NO_ENTITLEMENTS_PAYWALL',
+           isUserRegistered: isUserRegistered()
+         });
+         // B.3b) Show the paywall
+         showPaywall();
+       }
+     });
+   });
+
+   // B.2aiii) Handle the case when users click "Subscribe"
+   subscriptions.setOnNativeSubscribeRequest(() => showPaywall());
+  
+  }
+}
+
+
+/**
+ * Properties:
+ * - sku: Required. Sku to add to the user's subscriptions.
+ * - oldSku: Optional. This is if you want to replace one sku with another. For
+ *  example, if a user wants to upgrade or downgrade their current subscription.
+ * - prorationMode: Optional. When replacing a subscription you can decide on a
+ *  specific proration mode to charge the user.
+ *  The default is IMMEDIATE_WITH_TIME_PRORATION.
+ * - oneTime: Optional. When a user chooses a contribution, they have the option
+ *  to make it non-recurring.
+ *
+ *  @typedef {{
+ *    googleSignInClientId: string,
+ *    registrationEndpoint: string,
+ *    userState: Function,
+ *    onExtendedAccessGrant: Function,
+ *    onNoAccess: Function,
+ *    onLoginRequest: Function,
+ *    onSwGEntitlement: Function,
+ *    onNativeSubscribeRequest: Function,
+ * }}
+ */
+export let GaaMeteringParams;
