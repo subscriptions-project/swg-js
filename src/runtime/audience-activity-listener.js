463e80737c5afc7762edb94bedb05bdfa4cb62db
ksebina <sebina@google.com>
2021-12-08 15:26:10 -0500

    updates

--- src/runtime/audience-activity-listener.js
+++ src/runtime/audience-activity-listener.js
@@ -1,6 +1,18 @@
 
 /**
- * @fileoverview Description of this file.
+ * Copyright 2021 The Subscribe with Google Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
  import {
@@ -52,13 +64,18 @@
         const pubId = encodeURIComponent(this.deps_.pageConfig().getPublicationId());
         const audienceActivityClientLogsRequest = this.createLogRequest(event);
         const url = serviceUrl('/publication/' + pubId + '/audienceactivitylogs');
-        this.fetcher_.sendBeacon(url, audienceActivityClientLogsRequest);
+        if(this.fetcher_.sendBeacon(url, audienceActivityClientLogsRequest)) {
+          return;
+        }
+        else {
+          // failure
+        }
      }
    }
 
    /**
    * @param {!../api/client-event-manager-api.ClientEvent} event
-   * @return {!AudienceActivityLogRequest}
+   * @return {!AudienceActivityClientLogsRequest}
    */
    createLogRequest(event) {
       const request = new AudienceActivityClientLogsRequest();
