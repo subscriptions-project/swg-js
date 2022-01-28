a793356b1f2240751144f5d97e74014f492a92c9
Chris Antaki <cfa@google.com>
2022-01-28 11:50:14 -0800

    Story for Regwall

--- src/stories/Showcase.stories.js
+++ src/stories/Showcase.stories.js
@@ -1,8 +1,54 @@
-import {GaaGoogle3pSignInButton} from '../utils/gaa';
+import {GaaGoogle3pSignInButton, GaaMeteringRegwall} from '../utils/gaa';
 
 export default {
   title: 'Showcase',
 };
 
-export const ThirdPartySignInButton = () =>
-  GaaGoogle3pSignInButton.render('https://example.com/login');
+export const ThirdPartySignInButton = () => {
+  document.body.style.padding = '0';
+  return GaaGoogle3pSignInButton.render('https://example.com/login');
+};
+
+export const Regwall = () => {
+  // Add publisher metadata.
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "NewsArticle",
+  "headline": "16 Top Spots for Hiking",
+  "image": "https://scenic-2017.appspot.com/icons/icon-2x.png",
+  "datePublished": "2025-02-05T08:00:00+08:00",
+  "dateModified": "2025-02-05T09:20:00+08:00",
+  "author": {
+    "@type": "Person",
+    "name": "John Doe"
+  },
+  "publisher": {
+      "name": "The Scenic - USA",
+      "@type": "Organization",
+      "@id": "scenic-2017.appspot.com",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://scenic-2017.appspot.com/icons/icon-2x.png"
+      }
+  },
+  "description": "A most wonderful article",
+  "isAccessibleForFree": "False",
+  "isPartOf": {
+    "@type": ["CreativeWork", "Product"],
+    "name" : "Scenic News",
+    "productID": "scenic-2017.appspot.com:news"
+  }
+}
+</script>
+`
+  );
+
+  return GaaMeteringRegwall.render({
+    iframeUrl:
+      '/iframe.html?id=showcase--third-party-sign-in-button&viewMode=story',
+  });
+};
--- src/utils/gaa.js
+++ src/utils/gaa.js
@@ -485,11 +485,12 @@ export class GaaMeteringRegwall {
 
   /**
    * Renders the Regwall.
-   * @private
+   *
+   * Storybook uses this method for UI development.
    * @nocollapse
    * @param {{ iframeUrl: string, caslUrl: string }} params
    */
-  static render_({iframeUrl, caslUrl}) {
+  static render({iframeUrl, caslUrl}) {
     const languageCode = getLanguageCodeFromElement(self.document.body);
     const publisherName = GaaMeteringRegwall.getPublisherNameFromPageConfig_();
     const placeholderPatternForPublication = /<ph name="PUBLICATION".+?\/ph>/g;
@@ -584,6 +585,8 @@ export class GaaMeteringRegwall {
       const titleEl = self.document.getElementById(REGWALL_TITLE_ID);
       titleEl.focus();
     });
+
+    return containerEl;
   }
 
   /**
