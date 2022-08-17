f488cfbcc0e3af8c6c45d96920e9fb62af7e087d
justinchou-google <justinchou-google@users.noreply.github.com>
2022-08-17 20:15:37 +0000

    offers test

--- src/runtime/offers-flow-test.js
+++ src/runtime/offers-flow-test.js
@@ -276,6 +276,22 @@ describes.realWin('OffersFlow', {}, (env) => {
     await offersFlow.start();
   });
 
+  it('opens dialog with scrolling enabled when useUpdatedOfferFlows=true and allowScroll=true', async () => {
+    const clientConfigManager = runtime.clientConfigManager();
+    sandbox.stub(clientConfigManager, 'getClientConfig').resolves(
+      new ClientConfig({
+        useUpdatedOfferFlows: true,
+      })
+    );
+    sandbox.stub(clientConfigManager, 'shouldAllowScroll').returns(true);
+    offersFlow = new OffersFlow(runtime, {'isClosable': false});
+    dialogManagerMock
+      .expects('openView')
+      .withExactArgs(sandbox.match.any, false, {})
+      .once();
+    await offersFlow.start();
+  });
+
   it('should trigger on cancel', async () => {
     callbacksMock
       .expects('triggerFlowStarted')
