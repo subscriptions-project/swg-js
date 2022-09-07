1e4f97094e5e7e64205e488c141fdca4eba5ba0f
rbeckthomas <64608436+rbeckthomas@users.noreply.github.com>
2022-09-07 11:00:21 -0700

    Add experiment which disables miniprompt button for wide desktop screen. (#2382)
    * Add experiment which disables miniprompt button for wide desktop screen.
    
    * Update variable to const.
    
    * Add tests for mini prompt bypass feature.
    
    * Update formatting.
    
    * Fix formatting.
    
    * Remove accidental changes.
    
    * Add trailing whitespace.
    
    * Remove accidental text.
    
    * Fix CI errors.
    
    * Update object type in logging.
    
    * Fix lint errors.
    
    * Mark use of forbidden term innerWidth.
    
    * Move disabling logic to a helper function and return showPrompt_ to its
    original state.
    
    * Update the name of the disable miniprompt analytics event.
    
    * Respond to comments, adding comments and adding a test for logging.
    
    * Update comment styling and grammar.
    
    * Remove redundant test.
    
    * Add test for contribution miniprompt.
    
    * Run lint fix.
    
    * Updated comments for acccuracy.
    
    * Update log tests to use spy.
    
    * Remove unnecessary variable.
    
    * Make test expectation more concise.

--- src/runtime/auto-prompt-manager.js
+++ src/runtime/auto-prompt-manager.js
@@ -14,11 +14,13 @@
  * limitations under the License.
  */
 
-import {AnalyticsEvent} from '../proto/api_messages';
+import {AnalyticsEvent, EventOriginator} from '../proto/api_messages';
 import {AudienceActionFlow} from './audience-action-flow';
 import {AutoPromptType} from '../api/basic-subscriptions';
+import {ExperimentFlags} from './experiment-flags';
 import {MiniPromptApi} from './mini-prompt-api';
 import {assert} from '../utils/log';
+import {isExperimentOn} from './experiments';
 
 const STORAGE_KEY_IMPRESSIONS = 'autopromptimp';
 const STORAGE_KEY_DISMISSALS = 'autopromptdismiss';
@@ -53,6 +55,9 @@ export class AutoPromptManager {
   constructor(deps) {
     /** @private @const {!./deps.DepsDef} */
     this.deps_ = deps;
+
+    /** @private @const {!../model/doc.Doc} */
+    this.doc_ = deps.doc();
 
     /** @private @const {!../model/page-config.PageConfig} */
     this.pageConfig_ = deps.pageConfig();
@@ -89,6 +94,9 @@ export class AutoPromptManager {
 
     /** @private {?string} */
     this.promptDisplayed_ = null;
+
+    /** @private @const {!./client-event-manager.ClientEventManager} */
+    this.eventManager_ = deps.eventManager();
   }
 
   /**
@@ -121,7 +129,10 @@ export class AutoPromptManager {
   showAutoPrompt(params) {
     // Manual override of display rules, mainly for demo purposes.
     if (params.alwaysShow) {
-      this.showPrompt_(params.autoPromptType, params.displayLargePromptFn);
+      this.showPrompt_(
+        this.getPromptTypeToDisplay_(params.autoPromptType),
+        params.displayLargePromptFn
+      );
       return Promise.resolve();
     }
 
@@ -200,7 +211,11 @@ export class AutoPromptManager {
       }
       this.deps_.win().setTimeout(() => {
         this.autoPromptDisplayed_ = true;
-        this.showPrompt_(params.autoPromptType, promptFn);
+
+        this.showPrompt_(
+          this.getPromptTypeToDisplay_(params.autoPromptType),
+          promptFn
+        );
       }, (clientConfig?.autoPromptConfig.clientDisplayTrigger.displayDelaySeconds || 0) * SECOND_IN_MILLIS);
     });
   }
@@ -450,6 +465,54 @@ export class AutoPromptManager {
     ) {
       displayLargePromptFn();
     }
+  }
+
+  /**
+   * Returns which type of prompt to display based on the type specified,
+   * the viewport width, and whether the disableDesktopMiniprompt experiment
+   * is enabled.
+   *
+   * If the disableDesktopMiniprompt experiment is enabled and the desktop is
+   * wider than 480px then the large prompt type will be substituted for the mini
+   * prompt. The original promptType will be returned as-is in all other cases.
+   * @param {AutoPromptType|undefined} promptType
+   * @returns
+   */
+  getPromptTypeToDisplay_(promptType) {
+    const disableDesktopMiniprompt = isExperimentOn(
+      this.doc_.getWin(),
+      ExperimentFlags.DISABLE_DESKTOP_MINIPROMPT
+    );
+    const isWideDesktop = this.doc_.getWin()./* OK */ innerWidth > 480;
+
+    if (disableDesktopMiniprompt && isWideDesktop) {
+      if (promptType === AutoPromptType.SUBSCRIPTION) {
+        this.logDisableMinipromptEvent_(promptType);
+        return AutoPromptType.SUBSCRIPTION_LARGE;
+      }
+      if (promptType === AutoPromptType.CONTRIBUTION) {
+        this.logDisableMinipromptEvent_(promptType);
+        return AutoPromptType.CONTRIBUTION_LARGE;
+      }
+    }
+
+    return promptType;
+  }
+
+  /**
+   * Logs the disable miniprompt event.
+   * @param {AutoPromptType|undefined} overriddenPromptType
+   */
+  logDisableMinipromptEvent_(overriddenPromptType) {
+    this.eventManager_.logEvent({
+      eventType: AnalyticsEvent.EVENT_DISABLE_MINIPROMPT_DESKTOP,
+      eventOriginator: EventOriginator.SWG_CLIENT,
+      isFromUserAction: false,
+      additionalParameters: {
+        publicationid: this.pageConfig_.getPublicationId(),
+        promptType: overriddenPromptType,
+      },
+    });
   }
 
   /**
