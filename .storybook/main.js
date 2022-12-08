d532a0013dd38192bf46bfd4e8d6ececf2650171
Kristen W <kristenwang23@gmail.com>
2022-12-08 16:56:06 +0000

    automigrate storybook script

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,13 @@
 module.exports = {
-  'stories': [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  'stories': ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   'addons': ['@storybook/addon-links', '@storybook/addon-essentials'],
   'framework': '@storybook/html',
   'staticDirs': [
-    // HTML
-    '../src/stories/static',
-    // CSS
-    '../assets',
-  ],
+  // HTML
+  '../src/stories/static',
+  // CSS
+  '../assets'],
+  docs: {
+    docsPage: 'automatic'
+  }
 };
