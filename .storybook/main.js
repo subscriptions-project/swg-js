95d76c7cb5c695043cf7c4f478ad1181be93842d
Kristen W <kristenwang23@gmail.com>
2022-12-08 21:19:40 +0000

    Migrate to webpack 5 for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,20 @@
 module.exports = {
-  'stories': ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   'addons': ['@storybook/addon-links', '@storybook/addon-essentials'],
   'framework': '@storybook/html',
   'staticDirs': [
-  // HTML
-  '../src/stories/static',
-  // CSS
-  '../assets'],
+    // HTML
+    '../src/stories/static',
+    // CSS
+    '../assets',
+  ],
   docs: {
-    docsPage: 'automatic'
-  }
+    docsPage: 'automatic',
+  },
+  core: {
+    builder: 'webpack5',
+  },
 };
