9e180f7dcbaece3854b2f22c360cadbe33b4829b
Chris Antaki <cfa@google.com>
2023-01-06 19:47:03 +0000

    Move away from a few 3p deps

--- build-system/tasks/github.js
+++ build-system/tasks/github.js
@@ -15,8 +15,8 @@
  */
 'use strict';
 
-const BBPromise = require('bluebird');
-const request = BBPromise.promisify(require('request'));
+const {promisify} = require('node:util');
+const request = promisify(require('request'));
 
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 const GITHUB_BASE = 'https://api.github.com/repos/subscriptions-project/swg-js';
