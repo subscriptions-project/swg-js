cc45139d1513d73c2d2a9cce28692ab68185ba51
Chris Antaki <cfa@google.com>
2023-01-10 00:19:25 +0000

    Return undefined earlier

--- src/runtime/auto-prompt-manager.js
+++ src/runtime/auto-prompt-manager.js
@@ -189,12 +189,14 @@ export class AutoPromptManager {
       params.autoPromptType
     );
 
-    const potentialActionPromptType = await this.getAudienceActionPromptType_({
-      article,
-      autoPromptType: params.autoPromptType,
-      dismissedPrompts,
-      shouldShowAutoPrompt,
-    });
+    const potentialActionPromptType = article
+      ? await this.getAudienceActionPromptType_({
+          article,
+          autoPromptType: params.autoPromptType,
+          dismissedPrompts,
+          shouldShowAutoPrompt,
+        })
+      : undefined;
 
     const promptFn = potentialActionPromptType
       ? this.audienceActionPrompt_({
@@ -369,7 +371,7 @@ export class AutoPromptManager {
    * after the initial Contribution prompt. We also always default to showing the Contribution
    * prompt if the reader is currently inside of the frequency window, indicated by shouldShowAutoPrompt.
    * @param {{
-   *   article: (?./entitlements-manager.Article|undefined),
+   *   article: (!./entitlements-manager.Article),
    *   autoPromptType: (AutoPromptType|undefined),
    *   dismissedPrompts: (?string|undefined),
    *   shouldShowAutoPrompt: (boolean|undefined),
@@ -382,7 +384,7 @@ export class AutoPromptManager {
     dismissedPrompts,
     shouldShowAutoPrompt,
   }) {
-    const audienceActions = article?.audienceActions?.actions || [];
+    const audienceActions = article.audienceActions?.actions || [];
 
     // Count completed surveys.
     const [surveyCompletionTimestamps, surveyDataTransferFailureTimestamps] =
@@ -421,7 +423,7 @@ export class AutoPromptManager {
     ) {
       const preferSurveyOverContributionPrompt =
         await this.isExperimentEnabled_(
-          /** @type {!./entitlements-manager.Article} */ (article),
+          article,
           ExperimentFlags.SURVEY_TRIGGERING_PRIORITY
         );
 
