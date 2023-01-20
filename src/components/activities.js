e888bd9c27fe4dc5eb5e32d6b78b0d4d34fa1fff
Chris Antaki <cfa@google.com>
2023-01-20 19:36:46 +0000

    Organize imports

--- src/components/activities.js
+++ src/components/activities.js
@@ -13,18 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import activityPorts from 'web-activities/activity-ports';
 import {
   AnalyticsRequest,
   EventOriginator,
   deserialize,
   getLabel,
 } from '../proto/api_messages';
-import {INTERNAL_RUNTIME_VERSION} from '../constants';
-
 import {Constants} from '../utils/constants';
+import {INTERNAL_RUNTIME_VERSION} from '../constants';
 import {addQueryParam} from '../utils/url';
-
-import activityPorts from 'web-activities/activity-ports';
 
 const {
   ActivityIframePort: WebActivityIframePort,
