354a92c44c28141b11145821125dab3de6321988
Swit Tei <swit@google.com>
2023-02-02 14:00:58 +0800

    Fix unit test

--- src/runtime/extended-access/gaa-google3p-sign-in-button.js
+++ src/runtime/extended-access/gaa-google3p-sign-in-button.js
@@ -84,7 +84,7 @@ export class GaaGoogle3pSignInButton {
       });
 
       if (redirectMode) {
-        // TODO(b/242998655): Fix the downstream calls for logEvent to be chained to remove the need of delaying redirect.
+        // TODO(b/242998655): Fix the downstream calls for logEvent to be chained to remove the need of delaying redirect.        
         self.setTimeout(() => {
           self.open(authorizationUrl, '_parent');
         }, REDIRECT_DELAY);
