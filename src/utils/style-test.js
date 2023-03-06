b44cd38fe4aa0e5d632b0e473c6ada13dae7fd2e
Chris Antaki <cfa@google.com>
2023-03-06 09:18:01 -0800

    Simplify `src/utils/style` (#2806)
    * Simplify `src/utils/style`
    
    * Fix a test
    
    * Remove extra line
    
    * Cleanup import

--- src/utils/style-test.js
+++ src/utils/style-test.js
@@ -40,17 +40,11 @@ describes.realWin('Types', (env) => {
       expect(element.style.width).to.equal('1px');
     });
 
-    it('setStyle with vendor prefix', () => {
-      const element = {style: {WebkitTransitionDuration: ''}};
-      st.setStyle(element, 'transitionDuration', '1s', undefined, true);
-      expect(element.style.WebkitTransitionDuration).to.equal('1s');
-    });
-
     it('setStyles', () => {
       const element = doc.createElement('div');
       st.setStyles(element, {
-        width: st.px(101),
-        height: st.px(102),
+        width: '101px',
+        height: '102px',
       });
       expect(element.style.width).to.equal('101px');
       expect(element.style.height).to.equal('102px');
@@ -58,8 +52,8 @@ describes.realWin('Types', (env) => {
 
     it('getStyle handles missing property', () => {
       const element = doc.createElement('div');
-      const style = st.getStyle(element, '');
-      expect(style).to.be.undefined;
+      const style = st.getStyle(element, 'invalid-property');
+      expect(style).to.equal('');
     });
 
     it('resetAllStyles', () => {
@@ -68,93 +62,6 @@ describes.realWin('Types', (env) => {
       expect(element.style.objectFit).to.equal('fill');
       expect(element.style.opacity).to.equal('1');
       expect(element.style.display).to.equal('block');
-    });
-
-    it('px', () => {
-      expect(st.px(0)).to.equal('0px');
-      expect(st.px(101)).to.equal('101px');
-    });
-
-    it('translateX', () => {
-      expect(st.translateX(101)).to.equal('translateX(101px)');
-      expect(st.translateX('101vw')).to.equal('translateX(101vw)');
-    });
-
-    it('translate', () => {
-      expect(st.translate(101, 201)).to.equal('translate(101px, 201px)');
-      expect(st.translate('101vw, 201em')).to.equal('translate(101vw, 201em)');
-      expect(st.translate(101)).to.equal('translate(101px)');
-      expect(st.translate('101vw')).to.equal('translate(101vw)');
-    });
-
-    it('camelCaseToTitleCase', () => {
-      const str = 'theQuickBrownFox';
-      expect(st.camelCaseToTitleCase(str)).to.equal('TheQuickBrownFox');
-    });
-
-    describe('getVendorJsPropertyName', () => {
-      it('no prefix', () => {
-        const element = {style: {transitionDuration: ''}};
-        const prop = st.getVendorJsPropertyName(
-          element.style,
-          'transitionDuration',
-          true
-        );
-        expect(prop).to.equal('transitionDuration');
-      });
-
-      it('should use cached previous result', () => {
-        let element = {style: {transitionDuration: ''}};
-        let prop = st.getVendorJsPropertyName(
-          element.style,
-          'transitionDuration'
-        );
-        expect(prop).to.equal('transitionDuration');
-
-        element = {style: {WebkitTransitionDuration: ''}};
-        prop = st.getVendorJsPropertyName(element.style, 'transitionDuration');
-        expect(prop).to.equal('transitionDuration');
-      });
-
-      it('Webkit', () => {
-        const element = {style: {WebkitTransitionDuration: ''}};
-        const prop = st.getVendorJsPropertyName(
-          element.style,
-          'transitionDuration',
-          true
-        );
-        expect(prop).to.equal('WebkitTransitionDuration');
-      });
-
-      it('Moz', () => {
-        const element = {style: {MozTransitionDuration: ''}};
-        const prop = st.getVendorJsPropertyName(
-          element.style,
-          'transitionDuration',
-          true
-        );
-        expect(prop).to.equal('MozTransitionDuration');
-      });
-
-      it('O opera', () => {
-        const element = {style: {OTransitionDuration: ''}};
-        const prop = st.getVendorJsPropertyName(
-          element.style,
-          'transitionDuration',
-          true
-        );
-        expect(prop).to.equal('OTransitionDuration');
-      });
-
-      it('immediately returns properties with -- prefix', () => {
-        const prop = st.getVendorJsPropertyName({}, '--dashDash');
-        expect(prop).to.equal('--dashDash');
-      });
-
-      it('handles missing vendor prefixes', () => {
-        const prop = st.getVendorJsPropertyName({}, 'dashDash');
-        expect(prop).to.equal('dashDash');
-      });
     });
   });
 });
