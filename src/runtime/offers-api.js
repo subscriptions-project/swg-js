096174f8b1f35b2ec5d6611b8fa6f7863f795e8c
Chris Antaki <cfa@google.com>
2023-03-10 17:09:04 +0000

    Migrate `src/api/offer` to TypeScript

--- src/runtime/offers-api.js
+++ src/runtime/offers-api.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import {Offer} from '../api/offer';
 import {serviceUrl} from './services';
 
 export class OffersApi {
@@ -31,7 +32,7 @@ export class OffersApi {
 
   /**
    * @param {?string=} productId
-   * @return {!Promise<!Array<!../api/offer.Offer>>}
+   * @return {!Promise<!Array<!Offer>>}
    */
   getOffers(productId = this.config_.getProductId()) {
     if (!productId) {
@@ -42,7 +43,7 @@ export class OffersApi {
 
   /**
    * @param {string} productId
-   * @return {!Promise<!Array<!../api/offer.Offer>>}
+   * @return {!Promise<!Array<!Offer>>}
    * @private
    */
   async fetch_(productId) {
