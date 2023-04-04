7b0ff7740a1f1babdecac5596f5c6ee74f60310c
Chris Antaki <ChrisAntaki@gmail.com>
2023-04-04 12:57:57 -0700

    Remove a space lol

--- src/runtime/google-analytics-event-listener.ts
+++ src/runtime/google-analytics-event-listener.ts
@@ -81,7 +81,7 @@ export class GoogleAnalyticsEventListener {
     // additionalParameters isn't strongly typed so checking for both object and class notation.
     const subscriptionFlow =
       (event.additionalParameters as {[key: string]: string})?.[
-        'subscriptionFlow '
+        'subscriptionFlow'
       ] ||
       (event.additionalParameters as EventParams)?.getSubscriptionFlow?.() ||
       '';
