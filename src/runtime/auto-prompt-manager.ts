9dc7cd3fd015d064e640428ee59d929f226bf93b
Chris Antaki <ChrisAntaki@gmail.com>
2023-04-21 12:27:29 -0700

    Order imports

--- src/runtime/auto-prompt-manager.ts
+++ src/runtime/auto-prompt-manager.ts
@@ -22,12 +22,12 @@ import {
 } from './entitlements-manager';
 import {AudienceActionFlow, AudienceActionParams} from './audience-action-flow';
 import {AutoPromptType} from '../api/basic-subscriptions';
-// @ts-ignore: (b/276949133) Migrate to TypeScript.
-import {ConfiguredRuntime} from './runtime';
 import {ClientConfig} from '../model/client-config';
 import {ClientConfigManager} from './client-config-manager';
 import {ClientEvent} from '../api/client-event-manager-api';
 import {ClientEventManager} from './client-event-manager';
+// @ts-ignore: (b/276949133) Migrate to TypeScript.
+import {ConfiguredRuntime} from './runtime';
 import {Deps} from './deps';
 import {Doc} from '../model/doc';
 import {Entitlements} from '../api/entitlements';
