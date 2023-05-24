3320fc84d81d457761890817a5d6b46801341ab9
justinchou-google <justinchou-google@users.noreply.github.com>
2023-05-24 14:56:18 +0000

    fix test order to reflect expected article response

--- src/runtime/auto-prompt-manager-test.js
+++ src/runtime/auto-prompt-manager-test.js
@@ -1360,11 +1360,11 @@ describes.realWin('AutoPromptManager', (env) => {
         .resolves({
           audienceActions: {
             actions: [
+              SURVEY_INTERVENTION,
               {
                 type: 'TYPE_SUBSCRIPTION',
                 configurationId: 'subscription_config_id',
               },
-              SURVEY_INTERVENTION,
             ],
             engineId: '123',
           },
@@ -1448,7 +1448,6 @@ describes.realWin('AutoPromptManager', (env) => {
                 type: 'TYPE_SUBSCRIPTION',
                 configurationId: 'subscription_config_id',
               },
-              SURVEY_INTERVENTION,
             ],
             engineId: '123',
           },
