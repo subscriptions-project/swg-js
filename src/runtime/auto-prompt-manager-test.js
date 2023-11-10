362763e07a3c280c200929b0b3a15808e76bca64
Chris Antaki <cfa@google.com>
2023-11-10 10:34:53 -0800

    Simplify and improve coverage in `src/runtime/auto-prompt-manager.ts` (#3288)
    * Simplify and improve coverage in `src/runtime/auto-prompt-manager.ts`
    
    * Fix args to shouldShowMonetizationPromptAsSoftPaywall
    
    * Merge

--- src/runtime/auto-prompt-manager-test.js
+++ src/runtime/auto-prompt-manager-test.js
@@ -847,6 +847,24 @@ describes.realWin('AutoPromptManager', (env) => {
     });
   });
 
+  it('should not show monetization prompt as soft paywall if the type is undefined', async () => {
+    const shouldShow =
+      await autoPromptManager.shouldShowMonetizationPromptAsSoftPaywall(
+        undefined,
+        false
+      );
+    expect(shouldShow).to.be.false;
+  });
+
+  it('should not show monetization prompt as soft paywall if the type is NONE', async () => {
+    const shouldShow =
+      await autoPromptManager.shouldShowMonetizationPromptAsSoftPaywall(
+        AutoPromptType.NONE,
+        false
+      );
+    expect(shouldShow).to.be.false;
+  });
+
   it('should not display a prompt if the type is undefined', async () => {
     const entitlements = new Entitlements();
     entitlementsManagerMock
