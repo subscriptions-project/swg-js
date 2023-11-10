b437fe8218f8b8e83a924d5aa8faa7f87fac9ed7
Chris Antaki <cfa@google.com>
2023-11-10 10:49:26 -0800

    Fix typos

--- src/runtime/event-type-mapping-test.js
+++ src/runtime/event-type-mapping-test.js
@@ -122,7 +122,7 @@ describes.realWin('analyticsEventToGoogleAnalyticsEvent', () => {
     }
   });
 
-  it('uses subscriptionFlow param correclty on "subscription"', () => {
+  it('uses subscriptionFlow param correctly on "subscription"', () => {
     const actual = analyticsEventToGoogleAnalyticsEvent(
       AnalyticsEvent.ACTION_PAYMENT_COMPLETE,
       SubscriptionFlows.SUBSCRIBE
@@ -134,7 +134,7 @@ describes.realWin('analyticsEventToGoogleAnalyticsEvent', () => {
     expect(actual).to.be.equal(expected);
   });
 
-  it('uses subscriptionFlow param correclty on "contribution"', () => {
+  it('uses subscriptionFlow param correctly on "contribution"', () => {
     const actual = analyticsEventToGoogleAnalyticsEvent(
       AnalyticsEvent.ACTION_PAYMENT_COMPLETE,
       SubscriptionFlows.CONTRIBUTE
