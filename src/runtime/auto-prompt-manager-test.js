5ace47db0b5fe8ec5cee7fe437dc6c846021b4f6
justinchou-google <justinchou-google@users.noreply.github.com>
2024-01-23 01:21:24 +0000

    reverse commit

--- src/runtime/auto-prompt-manager-test.js
+++ src/runtime/auto-prompt-manager-test.js
@@ -3494,7 +3494,6 @@ describes.realWin('AutoPromptManager', (env) => {
       autoPromptConfig = new AutoPromptConfig({
         displayDelaySeconds: 0,
         numImpressionsBetweenPrompts: 2,
-
         dismissalBackOffSeconds: 5,
         maxDismissalsPerWeek: 2,
         maxDismissalsResultingHideSeconds: 10,
