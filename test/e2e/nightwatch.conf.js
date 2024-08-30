468d233004f8effbad404d05892742d5873bfd70
Michael Kawano <mhkawano@google.com>
2024-08-30 11:37:10 -0400

    up again

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -24,7 +24,7 @@ module.exports = {
     baseline_screenshots_path: 'test/e2e/vrt/baseline',
     latest_screenshots_path: '.vrt/latest',
     diff_screenshots_path: '.vrt/diff',
-    threshold: 0.05,
+    threshold: 0.1,
   },
 
   test_settings: {
