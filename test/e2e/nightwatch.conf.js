56f4c6b601d3848542424b90effb3ca84216cd79
Michael Kawano <mhkawano@google.com>
2024-09-12 10:56:34 -0400

    Undo threshold change

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -24,7 +24,7 @@ module.exports = {
     baseline_screenshots_path: 'test/e2e/vrt/baseline',
     latest_screenshots_path: 'vrt-report/latest',
     diff_screenshots_path: 'vrt-report/diff',
-    threshold: 0.0,
+    threshold: 0.02,
   },
 
   test_settings: {
