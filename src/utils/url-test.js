127cce8ac3bd082fe1cd95e36c9e3cb332ec9cd9
caroljli <caroldotli@gmail.com>
2024-09-20 11:26:23 -0400

    Update default value to null field for integration tests.

--- src/utils/url-test.js
+++ src/utils/url-test.js
@@ -271,7 +271,7 @@ describe('serializeProtoMessageForUrl', () => {
       ['Timestamp', 22222, 0],
       ['Duration', 100, 0],
       false,
-      'baseUrl',
+      null,
     ];
     const analyticsEventMetaArray = ['AnalyticsEventMeta', 1, true, null];
     const eventParamsArray = [
