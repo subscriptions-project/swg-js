060d70772eb4f833ac960fb33f03888c17b7e477
Kristen W <kristenwang23@gmail.com>
2024-10-19 00:27:02 +0000

    lint fix

--- src/api/available-intervention.ts
+++ src/api/available-intervention.ts
@@ -101,7 +101,7 @@ export class AvailableIntervention {
   async show(params: ShowInterventionParams): Promise<void> {
     if (
       this.intervention.type == InterventionType.TYPE_NEWSLETTER_SIGNUP ||
-      this.intervention.type == InterventionType.TYPE_REWARDED_SURVEY || 
+      this.intervention.type == InterventionType.TYPE_REWARDED_SURVEY ||
       this.intervention.type == InterventionType.TYPE_BYO_CTA
     ) {
       return new AudienceActionIframeFlow(this.deps_, {
