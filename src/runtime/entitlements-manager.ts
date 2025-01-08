007db7e82d3c8b5c1af75f33b8da993e66b3c10d
justinchou-google <justinchou-google@users.noreply.github.com>
2025-01-08 23:37:18 +0000

    remove locked queyr param

--- src/runtime/entitlements-manager.ts
+++ src/runtime/entitlements-manager.ts
@@ -812,7 +812,6 @@ export class EntitlementsManager {
       }
     }
 
-    url = addQueryParam(url, 'locked', String(this.pageConfig_.isLocked()));
     url = addQueryParam(
       url,
       'contentType',
