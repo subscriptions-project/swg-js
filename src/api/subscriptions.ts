58da0ef63aeb7fef7ac64e83842200b7ba9e8c78
Matt Borofsky <mborof@google.com>
2025-04-28 20:26:41 +0000

    remove unintended additions

--- src/api/subscriptions.ts
+++ src/api/subscriptions.ts
@@ -223,15 +223,6 @@ export interface Subscriptions {
   ): Promise<LinkSubscriptionResult>;
 
   /**
-   * Starts linking subscriptions for multiple publications in the same
-   * organization.
-   * @return promise indicating result of the operation
-   */
-  linkSubscriptions(
-    linkSubscriptionsRequest: LinkSubscriptionsRequest
-  ): Promise<LinkSubscriptionsResult>;
-
-  /**
    * Creates an element with the SwG button style and the provided callback.
    * The default theme is "light".
    */
