0ae54f06522b8622e8f8877b1556409c360e1990
Matt Borofsky <mborof@google.com>
2025-05-09 10:57:00 -0700

    Add partial support for linking multiple subscriptions (#3679)
    * Support multi-linking
    
    * support multiple links
    
    * fix tests
    
    * remove unsupported property
    
    * remove unsupported property
    
    * remove unintended additions
    
    * add runtime tests
    
    * subtle adjustments for coverage
    
    * allow null
    
    * add test for null list
    
    * respond to comments
    
    * use mocks more directly
    
    * reorder

--- src/runtime/subscription-linking-flow-test.js
+++ src/runtime/subscription-linking-flow-test.js
@@ -16,8 +16,12 @@
 
 import {ActivityIframeView} from '../ui/activity-iframe-view';
 import {ConfiguredRuntime} from './runtime';
+import {MockActivityPort} from '../../test/mock-activity-port';
 import {PageConfig} from '../model/page-config';
-import {SubscriptionLinkingCompleteResponse} from '../proto/api_messages';
+import {
+  SubscriptionLinkingCompleteResponse,
+  SubscriptionLinkingLinkResult,
+} from '../proto/api_messages';
 import {SubscriptionLinkingFlow} from './subscription-linking-flow';
 
 describes.realWin('SubscriptionLinkingFlow', (env) => {
@@ -30,13 +34,16 @@ describes.realWin('SubscriptionLinkingFlow', (env) => {
   let cancelCallback;
 
   const PUBLICATION_ID = 'pub1';
-  const REQUEST = {publisherProvidedId: 'ppid'};
+  let REQUEST;
+  let MULTI_REQUEST;
+  let activitiesMock;
 
   beforeEach(() => {
     win = env.win;
     pageConfig = new PageConfig(`${PUBLICATION_ID}:prod1`);
     runtime = new ConfiguredRuntime(win, pageConfig);
     dialogManagerMock = sandbox.mock(runtime.dialogManager());
+    activitiesMock = sandbox.mock(runtime.activities());
     messageMap = {};
     sandbox.stub(ActivityIframeView.prototype, 'on').callsFake((ctor, cb) => {
       const messageType = new ctor();
@@ -47,54 +54,104 @@ describes.realWin('SubscriptionLinkingFlow', (env) => {
       cancelCallback = cb;
     });
     subscriptionLinkingFlow = new SubscriptionLinkingFlow(runtime);
+    REQUEST = {publisherProvidedId: 'ppid'};
+    MULTI_REQUEST = {
+      linkTo: [
+        {publicationId: PUBLICATION_ID, publisherProvidedId: 'ppid'},
+        {publicationId: PUBLICATION_ID + '2', publisherProvidedId: 'ppid2'},
+      ],
+    };
   });
 
   describe('start', () => {
-    it('opens /linksaveiframe in an ActivityIframeView', async () => {
+    it('Setup the dialog and iframe properly', async () => {
       let activityIframeView;
       let hidden;
       let dialogConfig;
       dialogManagerMock
         .expects('openView')
         .once()
-        .callsFake((viewArg, hiddenArg, dialogConfigArg) => {
+        .callsFake(async (viewArg, hiddenArg, dialogConfigArg) => {
           activityIframeView = viewArg;
           hidden = hiddenArg;
           dialogConfig = dialogConfigArg;
-          return Promise.resolve();
         });
 
       subscriptionLinkingFlow.start(REQUEST);
 
-      const url = new URL(activityIframeView.src_);
-      const {pathname, searchParams} = url;
-      expect(pathname).to.equal('/swg/ui/v1/linksaveiframe');
-      expect(searchParams.get('subscriptionLinking')).to.equal('true');
-      expect(searchParams.get('ppid')).to.equal(REQUEST.publisherProvidedId);
-      const args = activityIframeView.args_;
-      expect(args['publicationId']).to.equal(PUBLICATION_ID);
-      expect(activityIframeView.shouldFadeBody_).to.be.false;
+      expect(activityIframeView.shouldFadeBody()).to.be.false;
       expect(hidden).to.be.false;
       expect(dialogConfig).to.deep.equal({
         desktopConfig: {isCenterPositioned: false},
       });
       dialogManagerMock.verify();
     });
+
+    it('opens /linksaveiframe in an ActivityIframeView', async () => {
+      const port = new MockActivityPort();
+      port.onResizeRequest = () => {};
+      port.whenReady = () => Promise.resolve();
+      activitiesMock
+        .expects('openIframe')
+        .withExactArgs(
+          sandbox.match((arg) => arg.tagName == 'IFRAME'),
+          `https://news.google.com/swg/ui/v1/linksaveiframe?_=_&subscriptionLinking=true&linkTo=${PUBLICATION_ID}%2C${REQUEST.publisherProvidedId}`,
+          {publicationId: PUBLICATION_ID, _client: 'SwG 0.0.0'}
+        )
+        .resolves(port);
+
+      subscriptionLinkingFlow.start(REQUEST);
+      await subscriptionLinkingFlow.getRenderPromise();
+
+      activitiesMock.verify();
+    });
+
+    it('throws an error if publisherProvidedId is missing', async () => {
+      const request = {...REQUEST, publisherProvidedId: undefined};
+      await expect(
+        subscriptionLinkingFlow.start(request)
+      ).to.eventually.be.rejectedWith('publisherProvidedId');
+    });
   });
 
-  it('throws an error if publisherProvidedId is missing', async () => {
-    const request = {...REQUEST, publisherProvidedId: undefined};
-    await expect(
-      subscriptionLinkingFlow.start(request)
-    ).to.eventually.be.rejectedWith('publisherProvidedId');
+  describe('startMultipleLinks', () => {
+    it('opens /linksaveiframe in an ActivityIframeView', async () => {
+      const port = new MockActivityPort();
+      port.onResizeRequest = () => {};
+      port.whenReady = () => Promise.resolve();
+      activitiesMock
+        .expects('openIframe')
+        .withExactArgs(
+          sandbox.match((arg) => arg.tagName == 'IFRAME'),
+          `https://news.google.com/swg/ui/v1/linksaveiframe?_=_&subscriptionLinking=true&linkTo=${PUBLICATION_ID}%2C${REQUEST.publisherProvidedId}&linkTo=${PUBLICATION_ID}2%2Cppid2`,
+          {publicationId: PUBLICATION_ID, _client: 'SwG 0.0.0'}
+        )
+        .resolves(port);
+
+      subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST);
+      await subscriptionLinkingFlow.getRenderPromise();
+
+      activitiesMock.verify();
+    });
+
+    it('throws an error if linkTo is missing', async () => {
+      await expect(
+        subscriptionLinkingFlow.startMultipleLinks({})
+      ).to.eventually.be.rejectedWith('linkTo');
+    });
   });
 
-  describe('on SubscriptionLinkingCompleteResponse', () => {
+  describe('singular on SubscriptionLinkingCompleteResponse', () => {
     it('resolves promise with response data', async () => {
       dialogManagerMock.expects('openView').once().resolves();
+      const linkResult = new SubscriptionLinkingLinkResult();
+      linkResult.setSuccess(true);
+      linkResult.setSwgPublicationId(PUBLICATION_ID);
+      linkResult.setPublisherProvidedId('abc');
       const response = new SubscriptionLinkingCompleteResponse();
-      response.setPublisherProvidedId('abc');
+      response.setLinkResultsList([linkResult]);
       response.setSuccess(true);
+      response.setPublisherProvidedId('abc');
 
       const resultPromise = subscriptionLinkingFlow.start(REQUEST);
       const handler = messageMap[response.label()];
@@ -106,8 +163,14 @@ describes.realWin('SubscriptionLinkingFlow', (env) => {
 
     it('resolves with success=false if missing from response', async () => {
       dialogManagerMock.expects('openView').once().resolves();
+      const linkResult = new SubscriptionLinkingLinkResult();
+      linkResult.setSwgPublicationId(PUBLICATION_ID);
+      linkResult.setPublisherProvidedId('abc');
+      linkResult.setSuccess(false);
       const response = new SubscriptionLinkingCompleteResponse();
       response.setPublisherProvidedId('abc');
+      response.setLinkResultsList([linkResult]);
+      response.setSuccess(false);
 
       const resultPromise = subscriptionLinkingFlow.start(REQUEST);
       const handler = messageMap[response.label()];
@@ -132,14 +195,144 @@ describes.realWin('SubscriptionLinkingFlow', (env) => {
     });
   });
 
+  describe('multiple on SubscriptionLinkingCompleteResponse', () => {
+    let expectedResults = [
+      new SubscriptionLinkingLinkResult(),
+      new SubscriptionLinkingLinkResult(),
+    ];
+    let expectedResponse = new SubscriptionLinkingCompleteResponse();
+    beforeEach(() => {
+      expectedResults = [
+        new SubscriptionLinkingLinkResult(),
+        new SubscriptionLinkingLinkResult(),
+      ];
+      expectedResults[0].setSuccess(true);
+      expectedResults[0].setSwgPublicationId(PUBLICATION_ID);
+      expectedResults[0].setPublisherProvidedId('ppid');
+      expectedResults[1].setSuccess(true);
+      expectedResults[1].setSwgPublicationId(PUBLICATION_ID + '2');
+      expectedResults[1].setPublisherProvidedId('ppid2');
+
+      expectedResponse = new SubscriptionLinkingCompleteResponse();
+      expectedResponse.setLinkResultsList(expectedResults);
+      expectedResponse.setSuccess(true);
+      expectedResponse.setPublisherProvidedId('ppid');
+    });
+
+    it('resolves promise with response data', async () => {
+      dialogManagerMock.expects('openView').once().resolves();
+
+      const resultPromise =
+        subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST);
+      const handler = messageMap[expectedResponse.label()];
+      handler(expectedResponse);
+
+      const result = await resultPromise;
+      expect(result).to.deep.equal({
+        anyFailure: false,
+        anySuccess: true,
+        links: [
+          {
+            publicationId: PUBLICATION_ID,
+            publisherProvidedId: 'ppid',
+            success: true,
+          },
+          {
+            publicationId: PUBLICATION_ID + '2',
+            publisherProvidedId: 'ppid2',
+            success: true,
+          },
+        ],
+      });
+    });
+
+    it('resolves with success=false on failure', async () => {
+      dialogManagerMock.expects('openView').once().resolves();
+
+      expectedResults[0].setSuccess(false);
+      // This ensures it defaults null to false.
+      expectedResults[1].setSuccess(null);
+      expectedResponse.setLinkResultsList(expectedResults);
+      expectedResponse.setSuccess(false);
+
+      const resultPromise =
+        subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST);
+      const handler = messageMap[expectedResponse.label()];
+      handler(expectedResponse);
+
+      const result = await resultPromise;
+      expect(result).to.deep.equal({
+        anyFailure: true,
+        anySuccess: false,
+        links: [
+          {
+            publicationId: PUBLICATION_ID,
+            publisherProvidedId: 'ppid',
+            success: false,
+          },
+          {
+            publicationId: PUBLICATION_ID + '2',
+            publisherProvidedId: 'ppid2',
+            success: false,
+          },
+        ],
+      });
+    });
+
+    it('handles no link results', async () => {
+      dialogManagerMock.expects('openView').once().resolves();
+      expectedResponse.setLinkResultsList(null);
+      expectedResponse.setSuccess(false);
+
+      const resultPromise =
+        subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST);
+      const handler = messageMap[expectedResponse.label()];
+      handler(expectedResponse);
+
+      const result = await resultPromise;
+      expect(result).to.deep.equal({
+        anyFailure: true,
+        anySuccess: false,
+        links: [],
+      });
+    });
+
+    it('throws an error when rejected', async () => {
+      dialogManagerMock
+        .expects('openView')
+        .once()
+        .rejects(new Error('Dialog error'));
+
+      await expect(
+        subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST)
+      ).to.eventually.be.rejectedWith('Dialog error');
+    });
+  });
+
   it('resolves promise with success=false when cancelled', async () => {
     dialogManagerMock.expects('openView').once().resolves();
 
-    const resultPromise = subscriptionLinkingFlow.start(REQUEST);
+    const resultPromise =
+      subscriptionLinkingFlow.startMultipleLinks(MULTI_REQUEST);
 
     cancelCallback();
 
     const result = await resultPromise;
-    expect(result).to.deep.equal({publisherProvidedId: 'ppid', success: false});
+    expect(result).to.deep.equal({
+      anySuccess: false,
+      anyFailure: true,
+      links: [
+        {
+          publicationId: PUBLICATION_ID,
+          publisherProvidedId: 'ppid',
+          success: false,
+        },
+        {
+          publicationId: PUBLICATION_ID + '2',
+          publisherProvidedId: 'ppid2',
+          success: false,
+        },
+      ],
+    });
   });
 });
