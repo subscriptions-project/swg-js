66a919253de758778796600db4a54b3ce2ee1fe1
Henrik Ellner <hellner@google.com>
2025-07-28 20:44:59 +0000

    implements Free Access API

---
+++ src/runtime/free-access-test.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {FreeAccess} from './free-access';
+import {LocationProvider} from './location-provider';
+
+
+describes.realWin('FreeAccess', () => {
+  describe('shouldAllowFreeAccess', () => {
+
+    let originalReferrerDescriptor;
+
+    beforeEach(() => {
+      // Save the original property descriptor.
+      originalReferrerDescriptor = Object.getOwnPropertyDescriptor(document, 'referrer');
+    });
+
+    afterEach(() => {
+      // Restore the original property descriptor to avoid side effects between tests.
+      if (originalReferrerDescriptor) {
+        Object.defineProperty(document, 'referrer', originalReferrerDescriptor);
+      }
+    });
+
+    it('dont allow for non-Google referer', () => {
+      const fakeReferrer = 'https://www.googlee.com/';
+      // Redefine the property
+      Object.defineProperty(document, 'referrer', {
+        value: fakeReferrer,
+        writable: false,     // Typically false for referrer
+        configurable: true  // MUST be true to allow a future redefinition/cleanup
+      });
+
+      const freeAccess = new FreeAccess();
+      expect(freeAccess.shouldAllowFreeAccess()).to.be.false;
+      expect(document.referrer).to.be.equal(fakeReferrer);
+    });
+    it('allow for Google referer', () => {
+      const fakeReferrer = 'https://www.google.com/';
+      // Redefine the property
+      Object.defineProperty(document, 'referrer', {
+        value: fakeReferrer,
+        writable: false,     // Typically false for referrer
+        configurable: true  // MUST be true to allow a future redefinition/cleanup
+      });
+
+      const freeAccess = new FreeAccess();
+      expect(freeAccess.shouldAllowFreeAccess()).to.be.true;
+      expect(document.referrer).to.be.equal(fakeReferrer);
+    });
+    it('dont allow for Google referer with explicit holdback', () => {
+      const fakeReferrer = 'https://www.google.com/';
+      // Redefine the property
+      Object.defineProperty(document, 'referrer', {
+        value: fakeReferrer,
+        writable: false,     // Typically false for referrer
+        configurable: true  // MUST be true to allow a future redefinition/cleanup
+      });
+      const mockLocation = sandbox.createStubInstance(LocationProvider);
+      mockLocation.getSearch.returns('?foo=bar&eafs_enabled=false');
+
+      const freeAccess = new FreeAccess(mockLocation);
+
+      expect(freeAccess.shouldAllowFreeAccess()).to.be.false;
+      expect(document.referrer).to.be.equal(fakeReferrer);
+    });
+    it('allow for Google referer with explicit no holdback', () => {
+      const fakeReferrer = 'https://www.google.com/';
+      // Redefine the property
+      Object.defineProperty(document, 'referrer', {
+        value: fakeReferrer,
+        writable: false,     // Typically false for referrer
+        configurable: true  // MUST be true to allow a future redefinition/cleanup
+      });
+      const mockLocation = sandbox.createStubInstance(LocationProvider);
+      mockLocation.getSearch.returns('?foo=bar&eafs_enabled=true');
+
+      const freeAccess = new FreeAccess(mockLocation);
+
+      expect(freeAccess.shouldAllowFreeAccess()).to.be.true;
+      expect(document.referrer).to.be.equal(fakeReferrer);
+    });
+  });
+});
---
+++ src/runtime/free-access.ts
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 The Subscribe with Google Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {FreeAccessApi} from '../api/free-access-api';
+import {LocationProvider} from './location-provider';
+
+export class FreeAccess implements FreeAccessApi {
+  private static readonly GOOGLE_REFERERS_PREFIX_LIST = [
+    'https://www.google.com/',
+    'android-app://com.google.android.googlequicksearchbox',
+    'android-app://com.google.android.googlequicksearchbox/https/www.google.com',
+    'ios-app://com.google.app.ios'];
+
+  private static readonly HOLDBACK_QUERY_PARAM = "eafs_enabled";
+  private static readonly HOLDBACK_ENABLED_VALUE = "false";
+
+  constructor(
+    private readonly locationProvider = new LocationProvider()) {}
+
+  /**
+  * Checks if free access is eligible to be granted to the user under the Free Access program.
+  * @return True if article free access regwall is eligible to be shown to the user.
+  */
+  shouldAllowFreeAccess() {
+    const googleReferer = FreeAccess.GOOGLE_REFERERS_PREFIX_LIST.some(prefix => document.referrer.toLowerCase().startsWith(prefix))
+    const queryMap = this.getQueryParamsAsMap();
+    const holdBackSet = queryMap.has(FreeAccess.HOLDBACK_QUERY_PARAM) && (queryMap.get(FreeAccess.HOLDBACK_QUERY_PARAM) === FreeAccess.HOLDBACK_ENABLED_VALUE);
+    return googleReferer && !holdBackSet;
+  }
+
+  returnLocation(): string {
+    return this.locationProvider.getSearch();
+  }
+
+  private getQueryParamsAsMap(): Map<string, string> {
+    const params = new URLSearchParams(this.locationProvider.getSearch());
+    const queryMap = new Map<string, string>();
+
+    for (const [key, value] of params.entries()) {
+      queryMap.set(key.toLowerCase(), value.toLowerCase());
+    }
+
+    return queryMap;
+  }
+}
