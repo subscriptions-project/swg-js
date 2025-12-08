8612f9a9eedc6366e4cbcf7285e97b7c59756e8a
Chris Antaki <cfa@google.com>
2025-12-08 10:24:09 -0800

    test: local server

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,9 +41,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- tests/e2e2/example.spec.ts
+++ tests/e2e2/example.spec.ts
@@ -1,15 +1,20 @@
 import {expect, test} from '@playwright/test';
 
+const URL = 'http://localhost:8000/examples/sample-pub/1';
+
 test('has title', async ({page}) => {
-  await page.goto('http://localhost:8080');
+  await page.goto(URL);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle('16 Top Spots for Hiking - The Scenic');
 });
 
 test('screenshot', async ({page}) => {
-  await page.goto('http://localhost:8080');
+  await page.goto(URL);
+
+  // Wait a bit.
+  await page.waitForTimeout(1000);
 
   // Compare screenshot.
-  await expect(page).toHaveScreenshot('homepage.png');
+  await expect(page).toHaveScreenshot('publication.png');
 });
