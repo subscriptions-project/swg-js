bb0237f8ec6870d531a13ae96e97f28a2914300f
Kristen Wang <kristenwang23@gmail.com>
2026-03-25 19:48:59 +0000

    update unit tests

--- src/runtime/auto-prompt-manager-test.js
+++ src/runtime/auto-prompt-manager-test.js
@@ -2168,6 +2168,16 @@ describes.realWin('AutoPromptManager', (env) => {
 
     it('should show the first CTA if the frequency cap is not met', async () => {
       expectFrequencyCappingTimestamps(storageMock, {
+        'TYPE_CONTRIBUTION': {
+          impressions: [
+            CURRENT_TIME -
+            10 * contributionFrequencyCapDurationSeconds * SECOND_IN_MS,
+          ],
+          dismissals: [
+            CURRENT_TIME -
+            10 * contributionFrequencyCapDurationSeconds * SECOND_IN_MS,
+          ],
+        },
         'contribution_config_id': {
           impressions: [
             CURRENT_TIME -
@@ -2187,6 +2197,12 @@ describes.realWin('AutoPromptManager', (env) => {
 
     it('should show the first CTA if the first CTA was abandoned', async () => {
       expectFrequencyCappingTimestamps(storageMock, {
+        'TYPE_CONTRIBUTION': {
+          impressions: [
+            CURRENT_TIME -
+            (funnelGlobalFrequencyCapDurationSeconds - 1) * SECOND_IN_MS,
+          ],
+        },
         'contribution_config_id': {
           impressions: [
             CURRENT_TIME -
@@ -3937,6 +3953,16 @@ describes.realWin('AutoPromptManager', (env) => {
 
       it('should show the first CTA if the frequency cap is not met', async () => {
         expectFrequencyCappingTimestamps(storageMock, {
+          'TYPE_CONTRIBUTION': {
+            impressions: [
+              CURRENT_TIME -
+              10 * contributionFrequencyCapDurationSeconds * SECOND_IN_MS,
+            ],
+            dismissals: [
+              CURRENT_TIME -
+              10 * contributionFrequencyCapDurationSeconds * SECOND_IN_MS,
+            ],
+          },
           'contribution_config_id': {
             impressions: [
               CURRENT_TIME -
