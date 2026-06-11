b8bbc2de8edee9623beaf6c2e5b3c4e54ac9b080
Henrik Ellner <hellner@google.com>
2026-06-11 16:51:56 +0000

    style fix

--- test/e2e/tests/enterpriseNewsletter.js
+++ test/e2e/tests/enterpriseNewsletter.js
@@ -22,7 +22,7 @@ module.exports = {
     basic
       .navigate()
       .pause(3000)
-      .assert.screenshotIdenticalToBaseline('html', 'enterprise-newsletter', { threshold: 0.1 })
+      .assert.screenshotIdenticalToBaseline('html', 'enterprise-newsletter', {threshold: 0.1})
       .viewNewsletter()
       .assert.textContains(
         '@newsletterHeader',
