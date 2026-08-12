6267d4dacdf0d88c76cdb374ea2185a65f67a675
minyuj <ryanighty@gmail.com>
2026-08-12 14:01:19 -0700

    Add mixRrmGisTokens utility to call backend action directly

--- src/runtime/gis/gis-utils.ts
+++ src/runtime/gis/gis-utils.ts
@@ -1,5 +1,11 @@
+import {Deps} from '../deps';
 import {GisInteropManager} from './gis-interop-manager';
 import {InterventionType} from '../../api/intervention-type';
+import {Message} from '../../proto/api_messages';
+import {StorageKeys} from '../../utils/constants';
+import {XhrFetcher} from '../fetcher';
+import {addQueryParam, parseUrl} from '../../utils/url';
+import {serviceUrl} from '../services';
 
 /**
  * The mode of the GIS.
@@ -33,4 +39,59 @@ export function getGisMode(
     return GisMode.GisModeOverlay;
   }
   return GisMode.GisModeNormal;
+}
+
+export interface MixRrmGisTokensParams {
+  idToken: string;
+  gisClientId: string;
+  gisOrigin?: string;
+}
+
+export interface MixRrmGisTokensResponse {
+  swgUserToken?: string;
+  [key: string]: unknown;
+}
+
+/**
+ * Calls MixRrmGisTokensAction directly to mix/link RRM and GIS tokens.
+ */
+export async function mixRrmGisTokens(
+  deps: Deps,
+  params: MixRrmGisTokensParams
+): Promise<MixRrmGisTokensResponse> {
+  const fetcher = new XhrFetcher(deps.win());
+  const publicationId = deps.pageConfig().getPublicationId();
+  const swgUserToken = await deps.storage().get(StorageKeys.USER_TOKEN, true);
+  const rrmOrigin = parseUrl(deps.win().location.href).origin;
+
+  const baseUrl = `/publication/${encodeURIComponent(
+    publicationId
+  )}/mixrrmgistokens`;
+
+  let url = serviceUrl(baseUrl);
+  if (swgUserToken) {
+    url = addQueryParam(url, 'sut', swgUserToken);
+  }
+  url = addQueryParam(url, 'id_token', params.idToken);
+  url = addQueryParam(url, 'gis_client_id', params.gisClientId);
+  url = addQueryParam(url, 'rrm_origin', rrmOrigin);
+  if (params.gisOrigin) {
+    url = addQueryParam(url, 'gis_origin', params.gisOrigin);
+  }
+
+  const emptyMessage: Message = {
+    toArray: () => [],
+    label: () => 'MixRrmGisTokens',
+  };
+
+  const response = (await fetcher.sendPost(
+    url,
+    emptyMessage
+  )) as MixRrmGisTokensResponse;
+
+  if (response?.swgUserToken) {
+    await deps.entitlementsManager().updateEntitlements(response.swgUserToken);
+  }
+
+  return response;
 }
